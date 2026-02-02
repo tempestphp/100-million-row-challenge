dc7dd5d6867f356450cfe5a46591bb5edce2c2a3
brendt <brent.roose@gmail.com>
2026-02-02 11:00:06 +0100

    wip

--- app/Parser.php
+++ app/Parser.php
@@ -2,12 +2,33 @@
 
 namespace App;
 
-use Exception;
+use DateTimeImmutable;
 
 final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $handle = fopen($inputPath, 'r');
+
+        $data = [];
+
+        while (($line = fgets($handle)) !== false) {
+            [$uri, $date] = explode(';', trim($line));
+
+            $date = new DateTimeImmutable($date);
+            $path = parse_url($uri, PHP_URL_PATH);
+
+            $data[$path][$date->format('Y-m-d')] ??= 0;
+
+            $data[$path][$date->format('Y-m-d')] += 1;
+        }
+
+        foreach ($data as &$visits) {
+            ksort($visits);
+        }
+
+        fclose($handle);
+
+        file_put_contents($outputPath, json_encode($data, JSON_PRETTY_PRINT));
     }
 }
