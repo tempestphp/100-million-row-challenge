21c98f7f9728289ce80d00d7c81a15c28d7140da
Dylan Bridgman <light@ether.org.za>
2026-02-24 15:34:40 +0200

    Parse the data

--- app/Parser.php
+++ app/Parser.php
@@ -8,6 +8,49 @@ final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        try {
+            $input = \fopen($inputPath, 'r');
+            $output = \fopen($outputPath, 'w');
+
+            $counters = [];
+
+            while ($line = \fgets($input)) {
+                $uri = substr($line, 19, -27);
+                $date = substr($line, -26, 10);
+
+                if (! isset($counters[$uri])) {
+                    $counters[$uri] = [$date => 0];
+                } elseif (! isset($counters[$uri][$date])) {
+                    $counters[$uri][$date] = 0;
+                }
+                $counters[$uri][$date]++;
+            }
+
+            $lastUri = null;
+            $lastDate = null;
+            \fwrite($output, "{");
+            foreach ($counters as $uri => $dates) {
+                ksort($dates);
+                foreach ($dates as $date => $count) {
+                    if ($lastUri !== $uri) {
+                        if ($lastUri !== null) {
+                            \fwrite($output, "\n    },");
+                        }
+                        $lastUri = $uri;
+                        $lastDate = null;
+                        \fwrite($output, "\n    \"". str_replace('/', '\\/', $uri) . "\": {");
+                    }
+                    if ($lastDate !== null) {
+                        \fwrite($output, ",");
+                    }
+                    $lastDate = $date;
+                    \fwrite($output, "\n        \"$date\": $count");
+                }
+            }
+            \fwrite($output, "\n    }\n}");
+        } finally {
+            @\fclose($input);
+            @\fclose($output);
+        }
     }
 }
