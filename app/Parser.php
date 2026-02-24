ceee66b457e6683a762f616769f2447d9f08be3f
kemo <kemal.delalic@gmail.com>
2026-02-24 15:19:55 +0100

    Add (fast) parsing

--- app/Parser.php
+++ app/Parser.php
@@ -2,12 +2,33 @@
 
 namespace App;
 
-use Exception;
-
 final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $counts = [];
+        $handle = fopen($inputPath, 'r');
+
+        set_error_handler(function () {});
+
+        while (($line = stream_get_line($handle, 256, "\n")) !== false) {
+            $counts[substr($line, 19, -15)]++;
+        }
+
+        restore_error_handler();
+        fclose($handle);
+
+        $data = [];
+        foreach ($counts as $key => $count) {
+            $commaPos = strrpos($key, ',');
+            $data[substr($key, 0, $commaPos)][substr($key, $commaPos + 1)] = $count;
+        }
+        unset($counts);
+
+        foreach ($data as &$dates) {
+            ksort($dates);
+        }
+
+        file_put_contents($outputPath, json_encode($data, JSON_PRETTY_PRINT));
     }
 }
