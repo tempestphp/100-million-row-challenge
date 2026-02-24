2d77e030d88744474315e684e607054aba5eed1c
MoeBrowne <moebrowne@users.noreply.github.com>
2026-02-24 15:42:21 +0000

    Adds parser implementation

--- app/Parser.php
+++ app/Parser.php
@@ -2,12 +2,35 @@
 
 namespace App;
 
-use Exception;
-
 final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $fp = fopen($inputPath, 'r');
+        stream_set_read_buffer($fp, 65536);
+
+        $data = [];
+
+        while (($line = stream_get_line($fp, 4096, "\n")) !== false) {
+            $commaPos = strpos($line, ',', 19);
+            $key = substr($line, 19, $commaPos + 11 - 19);
+
+            if (isset($data[$key])) {
+                $data[$key]++;
+            } else {
+                $data[$key] = 1;
+            }
+        }
+
+        $nestedData = [];
+        foreach ($data as $key => $count) {
+            $nestedData[substr($key, 0, -11)][substr($key, -10)] = $count;
+        }
+
+        foreach ($nestedData as &$values) {
+            ksort($values);
+        }
+
+        file_put_contents($outputPath, json_encode($nestedData, JSON_PRETTY_PRINT));
     }
 }
