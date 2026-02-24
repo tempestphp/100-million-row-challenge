52e366087b819d19c113a9e5c1882471073d96f7
Dylan Bridgman <light@ether.org.za>
2026-02-24 16:56:26 +0200

    Buffering and micro optimizations

--- app/Parser.php
+++ app/Parser.php
@@ -6,48 +6,76 @@ use Exception;
 
 final class Parser
 {
+    // 4MB read/write buffers.
+    const STREAM_BUFFER_SIZE = 4 << 20;
+
+    // A line is ±75 characters, so 75 * 2^14 = 1.2 MiB, which is a good buffer size for this input,
+    // so set a mask to quickly check when this many lines have been buffered.
+    const LINE_BUFFER_MASK = (1 << 14) - 1;
+
+    // Strip this prefix from every URI (length = 19).
+    private const URI_PREFIX_LEN = 19;    
+
     public function parse(string $inputPath, string $outputPath): void
     {
         try {
-            $input = \fopen($inputPath, 'r');
-            $output = \fopen($outputPath, 'w');
+            $input = \fopen($inputPath, 'rb');
+            $output = \fopen($outputPath, 'wb');
+
+            \stream_set_read_buffer($input,  self::STREAM_BUFFER_SIZE);
+            \stream_set_write_buffer($output, self::STREAM_BUFFER_SIZE);
 
             $counters = [];
 
             while ($line = \fgets($input)) {
-                $uri = substr($line, 19, -27);
-                $date = substr($line, -26, 10);
+                $comma = \strpos($line, ',', self::URI_PREFIX_LEN);
+                $uri  = \substr($line, self::URI_PREFIX_LEN, $comma - self::URI_PREFIX_LEN);
+                $date = \substr($line, $comma + 1, 10);                
 
-                if (! isset($counters[$uri])) {
-                    $counters[$uri] = [$date => 0];
-                } elseif (! isset($counters[$uri][$date])) {
-                    $counters[$uri][$date] = 0;
+                if (!isset($counters[$uri])) {
+                    $counters[$uri] = [];
                 }
-                $counters[$uri][$date]++;
+
+                $dates = &$counters[$uri];
+                if (isset($dates[$date])) {
+                    $dates[$date]++;
+                } else {
+                    $dates[$date] = 1;
+                }
             }
 
-            $lastUri = null;
-            $lastDate = null;
-            \fwrite($output, "{");
-            foreach ($counters as $uri => $dates) {
+            $buffer = "{";
+            $lines = 0;
+            $firstUri = true;
+            // Output validation requires that the URIs are the order they were first encountered in the input.
+            foreach ($counters as $uri => &$dates) {
+                if (!$firstUri) {
+                    $buffer .= ",";
+                }
+                $firstUri = false;
+
+                // Output validation requires that the URI slashes are escaped.
+                $buffer .= "\n    \"" . str_replace('/', '\\/', $uri) . "\": {";
+
+                // Output validation requires that the dates are in ascending order.
                 ksort($dates);
-                foreach ($dates as $date => $count) {
-                    if ($lastUri !== $uri) {
-                        if ($lastUri !== null) {
-                            \fwrite($output, "\n    },");
-                        }
-                        $lastUri = $uri;
-                        $lastDate = null;
-                        \fwrite($output, "\n    \"". str_replace('/', '\\/', $uri) . "\": {");
+
+                $firstDate = true;
+                foreach ($dates as $date => &$count) {
+                    if (!$firstDate) {
+                        $buffer .=  ",";
                     }
-                    if ($lastDate !== null) {
-                        \fwrite($output, ",");
+                    $firstDate = false;
+                    $buffer .=  "\n        \"$date\": $count";
+                    if ((++$lines & self::LINE_BUFFER_MASK) === 0) {
+                        \fwrite($output, $buffer);
+                        $buffer = '';
                     }
-                    $lastDate = $date;
-                    \fwrite($output, "\n        \"$date\": $count");
                 }
+                $buffer .= "\n    }";
             }
-            \fwrite($output, "\n    }\n}");
+            $buffer .= "\n}";
+            \fwrite($output, $buffer);
         } finally {
             @\fclose($input);
             @\fclose($output);
