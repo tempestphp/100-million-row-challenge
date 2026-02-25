03cc65756c591ccdd3baab2615199cccd86d0639
Robert Piringiu <robert.piringiu@emag.ro>
2026-02-25 22:54:29 +0200

    Implemented 100M challenge

--- app/Parser.php
+++ app/Parser.php
@@ -8,6 +8,28 @@ final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $file = fopen($inputPath, 'r');
+        $collector = [];
+        while (($line = fgets($file)) !== false) {
+            $comma = strpos($line, ',');
+            $firstKey = substr($line, 19, $comma-19);
+            $secondKey = substr($line, $comma + 1, -16);
+            if (!isset($collector[$firstKey][$secondKey])) {
+                $collector[$firstKey][$secondKey] = 0;
+            }
+            $collector[$firstKey][$secondKey]++;
+
+
+            unset($line);
+            unset($key);
+        }
+
+        fclose($file);
+
+        foreach ($collector as &$dates) {
+            ksort($dates);
+        }
+        
+        file_put_contents($outputPath, json_encode($collector, JSON_PRETTY_PRINT));
     }
 }
