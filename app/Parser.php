1be8ddd31e5ebe1a92d50b7c34c4355db3a0fba1
Hugo Alliaume <hugo@alliau.me>
2026-02-25 22:19:23 +0100

    Naive implementation

--- app/Parser.php
+++ app/Parser.php
@@ -8,6 +8,32 @@ final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $data = [];
+
+        // Read and iterate over the CSV file
+        foreach ($this->readCsv($inputPath) as [$url, $date]) {
+            $url = \str_replace('https://stitcher.io', '', $url);
+            $date = \substr($date, 0, 10);
+
+            $data[$url][$date] ??= 0;
+            $data[$url][$date]++;
+        }
+
+        // Sort the data for each URL
+        foreach ($data as &$urlData) {
+            \ksort($urlData);
+        }
+
+        // Write data
+        \file_put_contents($outputPath, \json_encode($data, JSON_PRETTY_PRINT));
+    }
+
+    private function readCsv(string $inputPath): iterable
+    {
+        $handle = \fopen($inputPath, "r");
+        while (($row = \fgetcsv($handle, escape: '\\')) !== false) {
+            yield $row;
+        }
+        \fclose($handle);
     }
 }
