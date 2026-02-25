c35b34111b9b226d3d9ccdb80d0d8e29491392f4
Sabri Taieb <sabritaiebdev@gmail.com>
2026-02-25 08:41:15 +0100

    my attempt

--- app/Parser.php
+++ app/Parser.php
@@ -3,11 +3,39 @@
 namespace App;
 
 use Exception;
+use Generator;
 
 final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $outputData = [];
+
+        foreach ($this->readLineByLine($inputPath) as $line) {
+            $parts = explode(',', $line);
+            $path = substr($parts[0], 19);
+            $date = substr($parts[1], 0, 10);
+
+            if (!isset($outputData[$path][$date])) {
+                $outputData[$path][$date] = 0;
+            }
+
+            $outputData[$path][$date]++;
+        }
+
+        $json = json_encode((object) $outputData);
+
+        file_put_contents($outputPath, $json);
+    }
+
+    private function readLineByLine(string $inputPath): Generator
+    {
+        $file = fopen($inputPath, 'r');
+
+        while ($line = fgets($file)) {
+            yield $line;
+        }
+
+        fclose($file);
     }
 }
