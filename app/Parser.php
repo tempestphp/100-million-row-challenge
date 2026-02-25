91b5653d871a085fd0e47d450a78130e66c842a2
Stefan <stefan@vrijdag.digital>
2026-02-25 15:15:43 +0100

    First quick and dirty setup 6s/10m

--- app/Parser.php
+++ app/Parser.php
@@ -2,12 +2,29 @@
 
 namespace App;
 
-use Exception;
+use SplFileObject;
 
 final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $result = [];
+
+        $file = new SplFileObject($inputPath);
+        while (!$file->eof()) {
+            if (preg_match('/^https:\/\/stitcher.io(.*),(.{10}).*$/', $file->current(), $matches)) {
+                $result[$matches[1]] ??= [];
+                $result[$matches[1]][$matches[2]] =
+                    ($result[$matches[1]][$matches[2]] ?? 0) + 1;
+            }
+
+            $file->next();
+        }
+
+        foreach ($result as &$row) {
+            ksort($row);
+        }
+
+        file_put_contents($outputPath, json_encode($result, JSON_PRETTY_PRINT));
     }
 }
