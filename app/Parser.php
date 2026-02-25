a334449aaa2b12e4a5f906ce4cc6ba3c2c8903b2
Nicklas Møller <hello@nicklasmoeller.com>
2026-02-25 18:44:36 +0100

    feat: parser

--- app/Parser.php
+++ app/Parser.php
@@ -2,12 +2,74 @@
 
 namespace App;
 
-use Exception;
-
 final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $handle = fopen($inputPath, 'r');
+
+        $visits = [];
+
+        while (! feof($handle)) {
+            $line = fgets($handle);
+            if (strpos($line, ',') === false) {
+                continue;
+            }
+
+            [$url, $timestamp] = explode(',', $line, 2);
+
+            [$date] = explode('T', $timestamp);
+
+            if (isset($visits[$url])) {
+                if (isset($visits[$url][$date])) {
+                    $visits[$url][$date]++;
+                } else {
+                    $visits[$url][$date] = 1;
+                }
+            } else {
+                $visits[$url] = [
+                    $date => 1,
+                ];
+            }
+        }
+
+        fclose($handle);
+
+        $write_handle = fopen($outputPath, 'w');
+
+        $segment = '{';
+
+        $first_line = true;
+
+        foreach ($visits as $url => $dates) {
+            ksort($dates);
+
+            [, $formatted_url] = explode('.io\/', str_replace('/', '\/', $url), 2);
+            if (! $first_line) {
+                $segment .= "\n    },\n    \"\/$formatted_url\": {";
+            } else {
+                $first_line = false;
+                $segment .= "\n    \"\/$formatted_url\": {";
+            }
+
+            $first_entry = true;
+            foreach ($dates as $date => $count) {
+                if (! $first_entry) {
+                    $segment .= ",\n        \"$date\": $count";
+                } else {
+                    $first_entry = false;
+                    $segment .= "\n        \"$date\": $count";
+                }
+            }
+
+            if (strlen($segment) >= 4096) {
+                fwrite($write_handle, $segment);
+                $segment = '';
+            }
+        }
+
+        $segment .= "\n    }\n}";
+        fwrite($write_handle, $segment);
     }
 }
+
