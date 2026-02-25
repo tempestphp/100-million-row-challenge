bedab436e20520b69bc4b50fee00995f0dfcab92
Stefan <stefan@vrijdag.digital>
2026-02-25 17:01:51 +0100

    Int indexed arrays 3.2s/10m

--- app/Parser.php
+++ app/Parser.php
@@ -6,8 +6,11 @@ final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        $result = [];
-        $endresult = [];
+        $url_map = [];
+        $time_map = [];
+        $url_counter = 0;
+        $time_counter = 0;
+        $counts = [];
 
         $handle = fopen($inputPath, 'r');
 
@@ -16,18 +19,41 @@ final class Parser
             $url = substr($line, 0, $pos);
             $timestamp = substr($line, $pos + 1, 10);
 
-            $result[$url] ??= [];
-            $result[$url][$timestamp] =
-                ($result[$url][$timestamp] ?? 0) + 1;
+            if (!isset($url_map[$url])) {
+                $url_map[$url] = $url_counter++;
+            }
+
+            if (!isset($time_map[$timestamp])) {
+                $time_map[$timestamp] = $time_counter++;
+            }
+
+            $uid = $url_map[$url];
+            $tid = $time_map[$timestamp];
+
+            if (!isset($counts[$uid])) {
+                $counts[$uid] = [];
+            }
+
+            $counts[$uid][$tid] = ($counts[$uid][$tid] ?? 0) + 1;
         }
 
         fclose($handle);
 
-        foreach ($result as $index => &$row) {
-            ksort($row);
-            $endresult[str_replace('https://stitcher.io','',$index)] = $row;
+        $map = [];
+
+        $id_to_url = array_flip($url_map);
+        $id_to_time = array_flip($time_map);
+
+        foreach ($counts as $uid => $time_counts) {
+            $url = str_replace('https://stitcher.io', '', $id_to_url[$uid]);
+
+            foreach ($time_counts as $tid => $count) {
+                $map[$url][$id_to_time[$tid]] = $count;
+            }
+
+            ksort($map[$url]);
         }
 
-        file_put_contents($outputPath, json_encode($endresult, JSON_PRETTY_PRINT));
+        file_put_contents($outputPath, json_encode($map, JSON_PRETTY_PRINT));
     }
 }
