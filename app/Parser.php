d8b3a6bb42ab2f8c18a4af19abb7c576c7d9231f
Aidan Casey <aidan@caseyhouse.net>
2026-02-24 23:42:55 -0500

    Initial commit

--- app/Parser.php
+++ app/Parser.php
@@ -2,12 +2,51 @@
 
 namespace App;
 
-use Exception;
+use SplFileObject;
 
 final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $inputFile = new SplFileObject($inputPath, 'r');
+        $inputFile->setCsvControl(',', '"', '');
+        $inputFile->setFlags(
+            SplFileObject::READ_CSV |
+            SPLFileObject::SKIP_EMPTY |
+            SplFileObject::DROP_NEW_LINE |
+            SPLFileObject::READ_AHEAD
+        );
+
+        $results = [];
+
+        while (! $inputFile->eof()) {
+            $row = $inputFile->fgetcsv();
+
+            if ($row === false || $row === [null]) {
+                continue;
+            }
+
+            $path = $this->parsePath($row[0]);
+            $date = substr($row[1], 0, 10);
+
+            $results[$path][$date] = 1 + ($results[$path][$date] ?? 0);
+        }
+
+        foreach ($results as &$path) {
+            ksort($path, SORT_STRING);
+        }
+
+        $json = json_encode($results, JSON_PRETTY_PRINT);
+
+        file_put_contents($outputPath, $json);
+    }
+
+    private function parsePath(string $url): string
+    {
+        $schemeStartPos = strpos($url, '://');
+        $schemeEndPos = ($schemeStartPos === false) ? 0 : $schemeStartPos + 3;
+        $firstSlashPos = strpos($url, '/', $schemeEndPos);
+
+        return substr($url, $firstSlashPos);
     }
 }
