351362089184bd65ad4d8ec2086507313e3f694d
Dylan Bridgman <light@ether.org.za>
2026-02-25 14:38:56 +0200

    More optimization

--- app/Parser.php
+++ app/Parser.php
@@ -21,6 +21,7 @@ final class Parser
 
     public function parse(string $inputPath, string $outputPath): void
     {
+        $time = \microtime(true);
         try {
             $input = \fopen($inputPath, 'rb');
             $output = \fopen($outputPath, 'wb');
@@ -29,6 +30,9 @@ final class Parser
             \stream_set_write_buffer($output, self::STREAM_BUFFER_SIZE);
 
             $counters = [];
+            $uris = [];
+            $uriIdSeq = 0;
+            $dates = [];
 
             $chunk = \fread($input, self::READ_CHUNK_SIZE);
             $start = 0;
@@ -36,9 +40,11 @@ final class Parser
                 // Try and find a line ending.
                 $lineEnd = \strpos($chunk, "\n", $start);
 
+                $notEndOfFile = !\feof($input);
+
                 // When there is no line ending, but more input to read, grab another chunk and append
                 // it to what is left of the current chunk.
-                if ($lineEnd === false && !\feof($input)) {
+                if ($lineEnd === false && $notEndOfFile) {
                     // No newline found, and we are not at the end of the file, so carry this chunk over to the next read.
                     $chunk = \substr($chunk, $start) . \fread($input, self::READ_CHUNK_SIZE);
                     $start = 0;
@@ -51,28 +57,38 @@ final class Parser
                 }
 
                 $uriStart = $start + self::URI_PREFIX_LEN;
+
+                // A comma is expected to be found here, because either a line ending or end of file has been found. No
+                // validation is done because the input format is expected to be correct.
                 $comma = \strpos($chunk, ',', $uriStart);
 
-                if ($comma !== false && $comma < $lineEnd) {
-                    $uri  = \substr($chunk, $uriStart, $comma - $uriStart);
-                    $date = \substr($chunk, $comma + 1, 10);
-                    if (!isset($counters[$uri][$date])) {
-                        $counters[$uri][$date] = 1;
-                    } else {
-                        $counters[$uri][$date] ++;
-                    }
+                // Convert the URI to an integer ID, to more efficiently append to the counters array.
+                $uriStr  = \substr($chunk, $uriStart, $comma - $uriStart);
+                $uriId = $uris[$uriStr] ??= $uriIdSeq++;
+
+                // Use the saved date string to avoid allocating a new string for every line.
+                $dateStr = \substr($chunk, $comma + 1, 10);
+                $date = $dates[$dateStr] ??= $dateStr;
+
+                if (!isset($counters[$uriId][$date])) {
+                    $counters[$uriId][$date] = 1;
+                } else {
+                    $counters[$uriId][$date]++;
                 }
 
                 $start = $lineEnd + 1;
 
-            } while ($start < \strlen($chunk) || !\feof($input));
+            } while ($notEndOfFile || $start < \strlen($chunk));
+
+            echo "Parsed in " . (\microtime(true) - $time) . " seconds.\n";
 
 
             $buffer = "{";
             $lines = 0;
             $firstUri = true;
             // Output validation requires that the URIs are the order they were first encountered in the input.
-            foreach ($counters as $uri => $dates) {
+            foreach ($uris as $uri => $uriId) {
+                $dates = $counters[$uriId];
                 if (!$firstUri) {
                     $buffer .= ",";
                 }
@@ -82,7 +98,7 @@ final class Parser
                 $buffer .= "\n    \"" . str_replace('/', '\\/', $uri) . "\": {";
 
                 // Output validation requires that the dates are in ascending order.
-                ksort($dates);
+                ksort($dates, \SORT_STRING);
 
                 $firstDate = true;
                 foreach ($dates as $date => $count) {
@@ -100,6 +116,8 @@ final class Parser
             }
             $buffer .= "\n}";
             \fwrite($output, $buffer);
+
+            echo "Written in " . (\microtime(true) - $time) . " seconds.\n";
         } finally {
             @\fclose($input);
             @\fclose($output);
