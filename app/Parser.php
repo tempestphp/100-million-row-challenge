681e99d7989bdd58c11ba5489f958f1440b5a948
Chester Swann-Auger <chester@hey.com>
2026-02-25 15:15:17 +0000

    Initial test

--- app/Parser.php
+++ app/Parser.php
@@ -8,6 +8,23 @@ final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $output = [];
+
+        if (($handle = fopen($inputPath, 'r')) !== false) {
+            while (($row = fgetcsv($handle, 0, ',', '"', '\\')) !== false) {
+                $url = substr($row[0], 19);
+                $date = substr($row[1], 0, 10);
+                $output[$url][$date] = ($output[$url][$date] ?? 0) + 1;
+            }
+            fclose($handle);
+        }
+
+        foreach ($output as &$dates) {
+            ksort($dates, SORT_STRING);
+        }
+
+        $output = json_encode($output, JSON_PRETTY_PRINT);
+
+        file_put_contents($outputPath, $output);
     }
 }
