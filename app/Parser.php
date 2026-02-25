77c92aa4a1945d422c0336bec28f60ad50a64865
fabpl <planchettefabrice@hotmail.com>
2026-02-25 17:18:08 +0100

    First attempt

--- app/Parser.php
+++ app/Parser.php
@@ -2,12 +2,28 @@
 
 namespace App;
 
-use Exception;
-
 final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $visitsByPath = [];
+
+        $handle = fopen($inputPath, 'r');
+
+        while (($line = fgets($handle)) !== false) {
+            [$url, $timestamp] = explode(',', trim($line));
+            $path = parse_url($url, PHP_URL_PATH);
+            $date = substr($timestamp, 0, 10);
+
+            $visitsByPath[$path][$date] = ($visitsByPath[$path][$date] ?? 0) + 1;
+        }
+
+        fclose($handle);
+
+        foreach ($visitsByPath as &$dates) {
+            ksort($dates);
+        }
+
+        file_put_contents($outputPath, json_encode($visitsByPath, JSON_PRETTY_PRINT));
     }
 }
