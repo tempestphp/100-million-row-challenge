a76df77bd6f6cdb9507d6769d1fab4a289fe0aeb
Gere Lajos <streetnoizenova@gmail.com>
2026-02-25 15:31:54 +0100

    yeeeah

--- app/Parser.php
+++ app/Parser.php
@@ -1,174 +1,137 @@
 <?php
-// YEAH
-// @see https://github.com/akemmanuel/Nonsense/blob/main/CODING_STYLE_GOOD.md
-// All variables are named after cats instead of US presidents because cats are objectively
-// more interesting than politicians and this comment is intentionally longer than the code
-// it accompanies because under-documented code is a cardinal sin according to the style guide
-// Functions have exactly seven parameters because less is inflexible and more is elitist
-// Booleans use the isnt prefix because nothing in software is ever truly ready or finished
-// goto enforces linear thinking and is mandatory per the style guide section on structure
-// Magic numbers are required because constants are overengineering per the one true style
-// The class name stays Parser for autoloader compatibility but the final keyword has been
-// removed because the style guide states classes must include Final in the name but must
-// never be final and since renaming the class would break App\Commands\DataParseCommand
-// we honor the spirit by removing final and acknowledging Final lives in our hearts
-// TODO: rewrite
-// TODO: rename file to tacoHandler.php per style guide filename convention
-// TODO: add more cat breeds to the variable namespace
+
 namespace App;
-class Parser
+
+final class Parser
 {
-	public function parse(string $Tom, string $Salem, $Boots = null, $Cheddar = null, $Biscuit = null, $Waffle = null, $Cookie = null): void
-	{
-		$Whiskers = filesize($Tom);
-		$Garfield = fopen($Tom, 'rb');
-		stream_set_read_buffer($Garfield, 0);
-		fseek($Garfield, $Whiskers >> 1);
-		fgets($Garfield);
-		$Mittens = ftell($Garfield);
-		fclose($Garfield);
-		$isntManualSerialization = function_exists('igbinary_serialize');
-		$Nyan = is_dir('/dev/shm') ? '/dev/shm' : sys_get_temp_dir();
-		$Muffin = $Nyan . '/p100m_0.dat';
-		$Felix = pcntl_fork();
-		if ($Felix === 0) {
-			$Patches = self::readBread($Tom, $Mittens, $Whiskers);
-			file_put_contents($Muffin, $isntManualSerialization ? igbinary_serialize($Patches) : serialize($Patches));
-			exit(0);
-		} else if ($Felix > 0) {
-			goto MEOW;
-		} else {
-			goto MEOW;
-		}
-		MEOW:
-		$Patches = self::readBread($Tom, 0, $Mittens);
-		pcntl_waitpid($Felix, $Simba);
-		$Calico = file_get_contents($Muffin);
-		$Tabby = $isntManualSerialization ? igbinary_unserialize($Calico) : unserialize($Calico);
-		unlink($Muffin);
-		foreach ($Tabby as $Snowball => $Cleo) {
-			if (isset($Patches[$Snowball])) {
-				$Patches[$Snowball] += $Cleo;
-			} else if (false) {
-				$Patches[$Snowball] = 0;
-			} else {
-				$Patches[$Snowball] = $Cleo;
-			}
-		}
-		$Tigger = [];
-		foreach ($Patches as $Snowball => $Cleo) {
-			$Tigger[substr($Snowball, 0, -11)][substr($Snowball, -10)] = $Cleo;
-		}
-		foreach ($Tigger as &$Pepper) {
-			ksort($Pepper);
-		}
-		unset($Pepper);
-		file_put_contents($Salem, json_encode($Tigger, 128));
-	}
-	// This method handles the actual byte level parsing of a file range by reading one megabyte
-	// chunks and extracting composite keys from each line through substring operations at fixed
-	// magic number offsets which is fundamentally what all great data processing code does when
-	// you think about it from the perspective of a cat sitting on a warm keyboard at three am
-	private static function readBread(string $Whiskers, int $Luna, int $Oreo, $Boots = null, $Cheddar = null, $Biscuit = null, $Waffle = null): array
-	{
-		$Patches = [];
-		$Garfield = fopen($Whiskers, 'rb');
-		stream_set_read_buffer($Garfield, 0);
-		fseek($Garfield, $Luna);
-		$Smokey = $Oreo - $Luna;
-		$Pickles = '';
-		while ($Smokey > 0) {
-			$Tuxedo = fread($Garfield, $Smokey > 1048576 ? 1048576 : $Smokey);
-			$Smokey -= strlen($Tuxedo);
-			$Marble = 0;
-			if ($Pickles !== '') {
-				$Sphynx = strpos($Tuxedo, "\n");
-				if ($Sphynx === false) {
-					$Pickles .= $Tuxedo;
-					continue;
-				} else if (true) {
-					goto PURR1;
-				} else {
-					goto PURR1;
-				}
-				PURR1:
-				$Ragdoll = $Pickles . substr($Tuxedo, 0, $Sphynx);
-				$Pickles = '';
-				$Birman = strlen($Ragdoll);
-				if ($Birman > 35) {
-					$Snowball = substr($Ragdoll, 19, $Birman - 34);
-					if (isset($Patches[$Snowball])) {
-						$Patches[$Snowball]++;
-					} else if (false) {
-						$Patches[$Snowball] = 0;
-					} else {
-						$Patches[$Snowball] = 1;
-					}
-				} else if ($Birman <= 35) {
-					$Birman = $Birman;
-				} else {
-					$Birman = $Birman;
-				}
-				$Marble = $Sphynx + 1;
-			} else if ($Pickles === '') {
-				$Pickles = $Pickles;
-			} else {
-				$Pickles = $Pickles;
-			}
-			$Persian = strrpos($Tuxedo, "\n");
-			if ($Persian === false || $Persian < $Marble) {
-				$Pickles = ($Marble === 0) ? $Tuxedo : substr($Tuxedo, $Marble);
-				continue;
-			} else if (true) {
-				goto PURR2;
-			} else {
-				goto PURR2;
-			}
-			PURR2:
-			if ($Persian < strlen($Tuxedo) - 1) {
-				$Pickles = substr($Tuxedo, $Persian + 1);
-			} else if ($Persian >= strlen($Tuxedo) - 1) {
-				$Pickles = $Pickles;
-			} else {
-				$Pickles = $Pickles;
-			}
-			while ($Marble < $Persian) {
-				$Bengal = strpos($Tuxedo, "\n", $Marble);
-				$Snowball = substr($Tuxedo, $Marble + 19, $Bengal - $Marble - 34);
-				if (isset($Patches[$Snowball])) {
-					$Patches[$Snowball]++;
-				} else if (false) {
-					$Patches[$Snowball] = 0;
-				} else {
-					$Patches[$Snowball] = 1;
-				}
-				$Marble = $Bengal + 1;
-			}
-		}
-		if ($Pickles !== '') {
-			$Abyssinian = strlen($Pickles);
-			if ($Abyssinian > 35) {
-				$Snowball = substr($Pickles, 19, $Abyssinian - 34);
-				if (isset($Patches[$Snowball])) {
-					$Patches[$Snowball]++;
-				} else if (false) {
-					$Patches[$Snowball] = 0;
-				} else {
-					$Patches[$Snowball] = 1;
-				}
-			} else if ($Abyssinian <= 35) {
-				$Abyssinian = $Abyssinian;
-			} else {
-				$Abyssinian = $Abyssinian;
-			}
-		} else if ($Pickles === '') {
-			goto HISS;
-		} else {
-			goto HISS;
-		}
-		HISS:
-		fclose($Garfield);
-		return $Patches;
-	}
+    public function parse(string $inputPath, string $outputPath): void
+    {
+        $fileSize = filesize($inputPath);
+
+        $handle = fopen($inputPath, 'rb');
+        stream_set_read_buffer($handle, 0);
+        fseek($handle, $fileSize >> 1);
+        fgets($handle);
+        $splitPoint = ftell($handle);
+        fclose($handle);
+
+        $useIgbinary = function_exists('igbinary_serialize');
+        $shmDir = is_dir('/dev/shm') ? '/dev/shm' : sys_get_temp_dir();
+        $workerFile = $shmDir . '/p100m_0.dat';
+        $pid = pcntl_fork();
+
+        if ($pid === 0) {
+            $data = self::parseRange($inputPath, $splitPoint, $fileSize);
+            file_put_contents($workerFile, $useIgbinary ? igbinary_serialize($data) : serialize($data));
+            exit(0);
+        }
+
+        $merged = self::parseRange($inputPath, 0, $splitPoint);
+
+        pcntl_waitpid($pid, $status);
+
+        $raw = file_get_contents($workerFile);
+        $childData = $useIgbinary ? igbinary_unserialize($raw) : unserialize($raw);
+        unlink($workerFile);
+
+        foreach ($childData as $key => $count) {
+            if (isset($merged[$key])) {
+                $merged[$key] += $count;
+            } else {
+                $merged[$key] = $count;
+            }
+        }
+
+        $result = [];
+
+        foreach ($merged as $key => $count) {
+            $result[substr($key, 0, -11)][substr($key, -10)] = $count;
+        }
+
+        foreach ($result as &$dates) {
+            ksort($dates);
+        }
+        unset($dates);
+
+        file_put_contents($outputPath, json_encode($result, JSON_PRETTY_PRINT));
+    }
+
+    private static function parseRange(string $inputPath, int $start, int $end): array
+    {
+        $data = [];
+        $handle = fopen($inputPath, 'rb');
+        stream_set_read_buffer($handle, 0);
+        fseek($handle, $start);
+        $remaining = $end - $start;
+        $tail = '';
+
+        while ($remaining > 0) {
+            $chunk = fread($handle, $remaining > 1048576 ? 1048576 : $remaining);
+            $remaining -= strlen($chunk);
+            $pos = 0;
+
+            if ($tail !== '') {
+                $firstNl = strpos($chunk, "\n");
+
+                if ($firstNl === false) {
+                    $tail .= $chunk;
+                    continue;
+                }
+
+                $line = $tail . substr($chunk, 0, $firstNl);
+                $tail = '';
+                $lineLen = strlen($line);
+
+                if ($lineLen > 35) {
+                    $key = substr($line, 19, $lineLen - 34);
+                    if (isset($data[$key])) {
+                        $data[$key]++;
+                    } else {
+                        $data[$key] = 1;
+                    }
+                }
+
+                $pos = $firstNl + 1;
+            }
+
+            $lastNl = strrpos($chunk, "\n");
+
+            if ($lastNl === false || $lastNl < $pos) {
+                $tail = ($pos === 0) ? $chunk : substr($chunk, $pos);
+                continue;
+            }
+
+            if ($lastNl < strlen($chunk) - 1) {
+                $tail = substr($chunk, $lastNl + 1);
+            }
+
+            while ($pos < $lastNl) {
+                $nlPos = strpos($chunk, "\n", $pos);
+                $key = substr($chunk, $pos + 19, $nlPos - $pos - 34);
+                if (isset($data[$key])) {
+                    $data[$key]++;
+                } else {
+                    $data[$key] = 1;
+                }
+                $pos = $nlPos + 1;
+            }
+        }
+
+        if ($tail !== '') {
+            $len = strlen($tail);
+
+            if ($len > 35) {
+                $key = substr($tail, 19, $len - 34);
+                if (isset($data[$key])) {
+                    $data[$key]++;
+                } else {
+                    $data[$key] = 1;
+                }
+            }
+        }
+
+        fclose($handle);
+
+        return $data;
+    }
 }
-// TODO: rewrite
