f5e8cce76cb2ac571aa7be46ca6c5d02fa614071
Salvatore Martire <4652631+salmart-dev@users.noreply.github.com>
2026-02-25 22:31:11 +0100

    feat: inital implementation

--- app/Parser.php
+++ app/Parser.php
@@ -8,6 +8,44 @@ final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $inFd = fopen($inputPath, 'r');
+        $arr = [];
+        $outFd = fopen($outputPath, 'w');
+        while(!feof($inFd)) {
+            $line = fgets($inFd);
+            $line = substr($line, 19, -1);
+            if ($line === '') {
+                break;
+            }
+            [$url, $time] = explode(',', $line, 2);
+            $date = substr($time, 0, 10);
+            [$year, $month, $day] = explode('-', $date);
+            $arr[$url] ??= [];
+            $arr[$url][$year] ??= [];
+            $arr[$url][$year][$month] ??= [];
+            $arr[$url][$year][$month][$day] ??= 0;
+            $arr[$url][$year][$month][$day]++;
+        }
+
+        $out = [];
+        foreach ($arr as $url => $data) {
+            $years = array_keys($data);
+            sort($years);
+            foreach ($years as $year) {
+                $months = array_keys($data[$year]);
+                sort($months);
+                foreach ($months as $month) {
+                    $days = array_keys($data[$year][$month]);
+                    sort($days);
+                    foreach ($days as $day) {
+                        $out[$url]["{$year}-{$month}-{$day}"] = $data[$year][$month][$day];
+                    }
+                }
+            }
+        }
+
+        echo memory_get_usage(true) / 1024 / 1024;
+
+        fwrite($outFd, json_encode($out, JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT));
     }
 }
