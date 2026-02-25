563f6c145ce9e4b16c7b949b414373dfefb31baa
Bram Ceulemans <bram@ceulemans.dev>
2026-02-25 22:31:19 +0000

    Add my solution

--- app/Parser.php
+++ app/Parser.php
@@ -2,12 +2,25 @@
 
 namespace App;
 
-use Exception;
-
 final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $handle = fopen($inputPath, 'r');
+
+        $output = [];
+        while (($line = fgets($handle)) !== false) {
+            $len = strlen($line);
+            $url = substr($line, 19, $len - 46);
+            $date = substr($line, $len - 26, 10);
+            $output[$url][$date] = ($output[$url][$date] ?? 0) + 1;
+        }
+
+        foreach ($output as &$dates) {
+            ksort($dates, SORT_STRING);
+        }
+
+        $outputHandle = fopen($outputPath, 'w');
+        fwrite($outputHandle, json_encode($output, JSON_PRETTY_PRINT));
     }
 }
