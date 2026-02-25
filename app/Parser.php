62cda8ff112c90e3b1cc33ec30343b0fc7d9e029
Salvatore Martire <4652631+salmart-dev@users.noreply.github.com>
2026-02-25 22:54:17 +0100

    feat: use workers

--- app/Parser.php
+++ app/Parser.php
@@ -1,51 +1,100 @@
 <?php
 
-namespace App;
+declare(strict_types=0);
 
-use Exception;
+namespace App;
 
 final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        $inFd = fopen($inputPath, 'r');
-        $arr = [];
-        $outFd = fopen($outputPath, 'w');
-        while(!feof($inFd)) {
-            $line = fgets($inFd);
-            $line = substr($line, 19, -1);
-            if ($line === '') {
-                break;
+        $children = 2;
+        $fstat = stat($inputPath);
+        $size = $fstat['size'];
+        $childSize = round($size / ($children));
+
+        $workers = [];
+        for($childNo = 1; $childNo <= $children; $childNo++) {
+            $socks = stream_socket_pair(STREAM_PF_UNIX, STREAM_SOCK_STREAM, 0);
+            $pid = pcntl_fork();
+
+            if ($pid === -1) {
+                exit('could not fork');
             }
-            [$url, $time] = explode(',', $line, 2);
-            $date = substr($time, 0, 10);
-            [$year, $month, $day] = explode('-', $date);
-            $arr[$url] ??= [];
-            $arr[$url][$year] ??= [];
-            $arr[$url][$year][$month] ??= [];
-            $arr[$url][$year][$month][$day] ??= 0;
-            $arr[$url][$year][$month][$day]++;
+
+            if ($pid === 0) {
+                // close parent sock
+                fclose($socks[0]);
+                // child
+                $start = ($childNo - 1) * $childSize;
+                $this->processFile($start, $childSize, $socks[1], $inputPath);
+                fclose($socks[1]);
+                exit(0);
+            }
+
+            // master
+            fclose($socks[1]);
+            $workers[] = ['pid' => $pid, 'sock' => $socks[0]];
         }
 
         $out = [];
-        foreach ($arr as $url => $data) {
-            $years = array_keys($data);
-            sort($years);
-            foreach ($years as $year) {
-                $months = array_keys($data[$year]);
-                sort($months);
-                foreach ($months as $month) {
-                    $days = array_keys($data[$year][$month]);
-                    sort($days);
-                    foreach ($days as $day) {
-                        $out[$url]["{$year}-{$month}-{$day}"] = $data[$year][$month][$day];
-                    }
-                }
+        foreach ($workers as $worker) {
+            $sock = $worker['sock'];
+            while (($line = fgets($sock)) !== false) {
+                $posU = strpos($line, "\t");
+                $posD = strpos($line, 'D', $posU);
+                $url = substr($line, 0, $posU);
+                $date = substr($line, $posU + 1, $posD - $posU - 1);
+                $count = (int)substr($line, $posD + 1);
+
+                $out[$url][$date] = ($out[$url][$date] ?? 0 ) + $count;
             }
         }
 
-        echo memory_get_usage(true) / 1024 / 1024;
+        foreach ($out as &$data) {
+            ksort($data);
+        }
+        unset($data);
 
-        fwrite($outFd, json_encode($out, JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT));
+        $outFd = fopen($outputPath, 'w');
+        fwrite($outFd, json_encode($out, JSON_PRETTY_PRINT));
+        fflush($outFd);
+        fclose($outFd);
     }
+
+    private function processFile(int $start, int $size, mixed $outSock, string $fileIn): void
+    {
+        $inFd = fopen($fileIn, 'r');
+        $seekResult = fseek($inFd, $start);
+        if ($seekResult === -1) {
+            exit('could not seek');
+        }
+
+        if ($start !== 0) {
+            // skip an incomplete line
+            fgets($inFd);
+        }
+
+        $arrByUrlAndDate = [];
+        $currPos = ftell($inFd);
+        $limit = $start + $size;
+        while(($line = fgets($inFd)) !== false) {
+            $commaPos = strpos($line, ',');
+            $url = substr($line, 19, $commaPos - 19);
+            $date = substr($line, $commaPos + 1, 10);
+            $arrByUrlAndDate[$url][$date] = ($arrByUrlAndDate[$url][$date] ?? 0) + 1;
+            $currPos += strlen($line);
+            if ($currPos > $limit) {
+                break;
+            }
+        }
+        fclose($inFd);
+
+        foreach ($arrByUrlAndDate as $url => $data) {
+            foreach ($data as $date => $count) {
+                fwrite($outSock, "{$url}\t{$date}D{$count}\n");
+            }
+        }
+    }
+
 }
