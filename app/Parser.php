0c28be677df5f8bee7e2e4fcf3bdd5bb535bcf00
Côme Chilliet <come@chilliet.eu>
2026-02-26 00:19:44 +0100

    First working version
    Signed-off-by: Côme Chilliet <come@chilliet.eu>

--- app/Parser.php
+++ app/Parser.php
@@ -6,8 +6,34 @@ use Exception;
 
 final class Parser
 {
+    const PATH_PREFIX_LENGTH = 19;
+    const DATE_LENGTH = 10;
+    const TIME_LENGTH = 15;
+    const DATETIME_LENGTH = self::DATE_LENGTH + self::TIME_LENGTH;
+
+
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $data = [];
+        foreach($this->parseInput($inputPath) as $url => $date) {
+            if (isset($data[$url][$date])) {
+                $data[$url][$date]++;
+            } else {
+                $data[$url][$date] = 1;
+            }
+        }
+        array_walk(
+            $data,
+            static fn(&$dates) => ksort($dates)
+        );
+        file_put_contents($outputPath, json_encode($data, flags:JSON_PRETTY_PRINT));
+    }
+
+    private function parseInput(string $inputPath) {
+        $r = fopen($inputPath, 'r');
+        $data = [];
+        while (($line = fgets($r)) !== false) {
+            yield substr($line, self::PATH_PREFIX_LENGTH, -self::DATETIME_LENGTH - 2) => substr($line, -self::DATETIME_LENGTH - 1, self::DATE_LENGTH);
+        }
     }
 }
