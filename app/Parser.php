92d4684fbceda9f9c50677520a5ad72dd002bf88
gordonmcvey <grj.mcvey@gmail.com>
2026-02-26 19:29:37 +0000

    Benchmarking explode vs strpos and substr

--- app/Parser.php
+++ app/Parser.php
@@ -1,38 +1,67 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App;
 
 use Exception;
+use Generator;
+use function explode;
+use function fclose;
+use function feof;
+use function fgets;
+use function fopen;
+use function fread;
+use function str_ends_with;
 
 final class Parser
 {
+    private const int READ_CHUNK_SIZE = 1024 * 1024 * 16;
+
+    // "https://stitcher.io" is 19 chars long
+    private const int URL_HOST_LEN = 19;
+    /**
+     * @var false|resource
+     */
+    private $inputHandle;
+
     /**
      * @throws Exception
      */
     public function parse(string $inputPath, string $outputPath): void
     {
         /** @var resource $inputHandle */
-        ($inputHandle = \fopen($inputPath, "r")) || throw new \Exception("Couldn't open input file");
-//        $this->fGetsCsvMethod($inputHandle);
-        $this->fReadMethod($inputHandle);
-        \fclose($inputHandle);
-    }
-
-    private function fGetsCsvMethod($handle): void
-    {
-        while(!\feof($handle)) {
-            $arr = \fgetcsv($handle, 1024, escape: "\\");
+        try {
+            $data = [];
+            ($this->inputHandle = fopen($inputPath, "r")) || throw new Exception("Couldn't open input file");
+            foreach ($this->getChunk() as $chunk) {
+                foreach($chunk as $row) {
+                    if(!$row) {
+                        continue;
+                    }
+//                    $separatorPos = \strpos($row, ",", self::URL_HOST_LEN);
+//                    $path = substr($row, self::URL_HOST_LEN, $separatorPos - self::URL_HOST_LEN);
+//                    $date = substr($row, $separatorPos + 1, 10);
+                    $parts = explode(",", $row);
+//                    print_r([$row, $path, $date]);
+                }
+            }
+        } finally {
+            !$this->inputHandle || fclose ($this->inputHandle);
         }
     }
 
-    private function fReadMethod($handle): void
+    /**
+     * @return Generator<array<int, string>>
+     */
+    private function getChunk(): Generator
     {
-        while(!feof($handle)) {
-            $str = \fread($handle, 1024 * 1024 * 8);
-            if (!\str_ends_with($str, "\n")) {
-                $str .= \fgets($handle, 1024);
+        while(!feof($this->inputHandle)) {
+            $str = fread($this->inputHandle, self::READ_CHUNK_SIZE);
+            if (!str_ends_with($str, "\n")) {
+                $str .= (string) fgets($this->inputHandle, 1024);
             }
-            $arr = \explode("\n", $str);
+            yield explode("\n", $str);
         }
     }
 }
