18f2bd6888fe7daeb6861e136086d9a9d55de251
gordonmcvey <grj.mcvey@gmail.com>
2026-02-26 18:05:40 +0000

    Benchmarking file reading methods

--- app/Parser.php
+++ app/Parser.php
@@ -6,8 +6,33 @@ use Exception;
 
 final class Parser
 {
+    /**
+     * @throws Exception
+     */
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        /** @var resource $inputHandle */
+        ($inputHandle = \fopen($inputPath, "r")) || throw new \Exception("Couldn't open input file");
+//        $this->fGetsCsvMethod($inputHandle);
+        $this->fReadMethod($inputHandle);
+        \fclose($inputHandle);
+    }
+
+    private function fGetsCsvMethod($handle): void
+    {
+        while(!\feof($handle)) {
+            $arr = \fgetcsv($handle, 1024, escape: "\\");
+        }
+    }
+
+    private function fReadMethod($handle): void
+    {
+        while(!feof($handle)) {
+            $str = \fread($handle, 1024 * 1024 * 8);
+            if (!\str_ends_with($str, "\n")) {
+                $str .= \fgets($handle, 1024);
+            }
+            $arr = \explode("\n", $str);
+        }
     }
 }
