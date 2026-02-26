0483d4cc2bcb72f746595010cc2b046c37282651
Ayesh Karunaratne <ayesh@aye.sh>
2026-02-26 18:18:10 +0530

    Add entry from @ayesh

--- app/Parser.php
+++ app/Parser.php
@@ -8,6 +8,21 @@ final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $in = \file_get_contents($inputPath);
+        \preg_match_all('@^(?:https://stitcher\.io)(.+?),(202[\d:+-]+)(?:T.+)$@m', $in, $matches, \PREG_SET_ORDER);
+
+        $data = [];
+
+        foreach ($matches as $match) {
+            $path = $match[1];
+            $date = $match[2]; // list / [] is somehow slower
+            $data[$path][$date] = ($data[$path][$date] ?? 0) + 1;
+        }
+
+        foreach ($data as &$dates) {
+            \ksort($dates);
+        }
+
+        \file_put_contents($outputPath, \json_encode($data, \JSON_PRETTY_PRINT));
     }
 }
