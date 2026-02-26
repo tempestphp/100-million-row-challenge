c342710d894fce6d16f2c18d610ae6838978bda5
Pavel Iakimov <pavel.iakimov@manychat.com>
2026-02-26 22:47:02 +0100

    int keys instead of dates

--- app/Parser.php
+++ app/Parser.php
@@ -4,9 +4,13 @@ namespace App;
 
 use Exception;
 
+use function substr;
+
 final class Parser
 {
     private bool $isDebug;
+    private array $map = [];
+    private array $revMap = [];
 
     public function __construct()
     {
@@ -24,25 +28,26 @@ final class Parser
         $this->timer('open');
         $prev = '';
         while ($data = fread($input, 1024 * 16)) {
-            $prevLines = explode("\n", $prev);
             $lines = explode("\n", $data);
+            $lines[0] = $prev . $lines[0];
             $prev = array_pop($lines);
-            $lines[0] = array_pop($prevLines) . $lines[0];
-            foreach ($prevLines as $line) {
-                $url = substr($line, 19, -26);
-                $date = substr($line, -25, -15);
-                $result[$url][$date] = ($result[$url][$date] ?? 0) + 1;
-            }
             foreach ($lines as $line) {
-                $url = substr($line, 19, -26);
-                $date = substr($line, -25, -15);
-                $result[$url][$date] = ($result[$url][$date] ?? 0) + 1;
+                $url = substr($line, 25, -26);
+                $date = substr($line, -23, -15);
+                $key = $this->getKey($date);
+                $result[$url][$key] = ($result[$url][$key] ?? 0) + 1;
             }
         }
         $this->timer('parse');
 
-        foreach ($result as $key => $value) {
-            ksort($result[$key]);
+        foreach ($result as $uri => $value) {
+            unset($result[$uri]);
+            foreach ($value as $key => $count) {
+                unset($value[$key]);
+                $value[$this->getDate($key)] = $count;
+            }
+            ksort($value);
+            $result["/blog/$uri"] = $value;
         }
         $this->timer('sort');
 
@@ -64,5 +69,20 @@ final class Parser
 
         echo "$name:\t" . ($current - $last) . "\n";
         $last = $current;
+    }
+
+    private function getKey(string $date): int
+    {
+        if (isset($this->map[$date])) {
+            return $this->map[$date];
+        }
+
+        $this->revMap[] = $date;
+        return $this->map[$date] = count($this->revMap) - 1;
+    }
+
+    private function getDate(int $key): string
+    {
+        return "20{$this->revMap[$key]}";
     }
 }
