b3f583d5cfa970ea08ee58a22d563cbc699216ad
Mohsen Shamohammadi <mohsen.shamohammadi@atolls.com>
2026-02-26 23:51:02 +0100

    my version

--- app/Parser.php
+++ app/Parser.php
@@ -8,6 +8,33 @@ final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        // Read the input file line by line
+        $inputFile = fopen($inputPath, 'r');
+        if (!$inputFile) {
+            throw new Exception("Could not open the input file: $inputPath");
+        }
+
+        $urls = [];
+
+        while (($line = fgets($inputFile)) !== false) {
+            $pos = strpos($line, ',');
+            $url = substr($line, 19, $pos - 19);
+            $date = substr($line, $pos + 1, 10);
+            if(isset($urls[$url][$date])) {
+                $urls[$url][$date]++;
+            }
+            else{
+                $urls[$url][$date] = 1;
+            }
+        }
+
+        // Sort dates for each URL by lexicographical order (which works for YYYY-M-D format)
+        foreach ($urls as &$dates) {
+            ksort($dates);
+        }
+
+        file_put_contents($outputPath,
+            json_encode($urls, JSON_PRETTY_PRINT )
+        );
     }
 }
