5e91abf6710fab9970ff68f9410f2c6bef2f6442
Joe Campo <jcampo@gmail.com>
2026-02-26 18:44:43 -0500

    Update parser

--- app/Parser.php
+++ app/Parser.php
@@ -4,16 +4,16 @@ namespace App;
 
 final class Parser
 {
-    private const CHUNK_SIZE  = 32 * 1024 * 1024;
-    private const NUM_WORKERS = 2;
+    private const CHUNK_SIZE = 64 * 1024 * 1024;
+    private const NUM_WORKERS = 8;
 
     public function parse(string $inputPath, string $outputPath): void
     {
-        $size   = filesize($inputPath);
+        $size = filesize($inputPath);
         $bounds = $this->splitBoundaries($inputPath, $size);
 
         $tmp = sys_get_temp_dir() . '/parser_' . getmypid() . '_';
-        $id  = 0;
+        $id = 0;
 
         for ($i = 1; $i < self::NUM_WORKERS; $i++) {
             $pid = pcntl_fork();
@@ -31,7 +31,7 @@ final class Parser
         while (pcntl_waitpid(-1, $status) > 0) {}
 
         for ($i = 1; $i < self::NUM_WORKERS; $i++) {
-            $f     = $tmp . $i;
+            $f = $tmp . $i;
             $child = igbinary_unserialize(file_get_contents($f));
             unlink($f);
 
@@ -43,7 +43,7 @@ final class Parser
                 foreach ($dates as $date => $count) {
                     isset($visits[$url][$date])
                         ? ($visits[$url][$date] += $count)
-                        : ($visits[$url][$date]  = $count);
+                        : ($visits[$url][$date] = $count);
                 }
             }
         }
@@ -57,7 +57,7 @@ final class Parser
     private function splitBoundaries(string $path, int $size): array
     {
         $fh = fopen($path, 'rb');
-        $b  = [0];
+        $b = [0];
 
         for ($i = 1; $i < self::NUM_WORKERS; $i++) {
             fseek($fh, (int)($size * $i / self::NUM_WORKERS));
@@ -76,31 +76,46 @@ final class Parser
         fseek($fh, $start);
 
         $visits = [];
-        $carry  = '';
-        $left   = $end - $start;
+        $carry = '';
+        $left = $end - $start;
 
         while ($left > 0) {
             $raw = fread($fh, min(self::CHUNK_SIZE, $left));
             if ($raw === false || $raw === '') break;
-            $left -= strlen($raw);
+            $rawLen = strlen($raw);
+            $left -= $rawLen;
 
-            $buf = $carry . $raw;
-
-            if (($nl = strrpos($buf, "\n")) === false) { $carry = $buf; continue; }
-
-            $carry  = substr($buf, $nl + 1);
             $offset = 0;
 
-            while (($nl = strpos($buf, "\n", $offset)) !== false) {
-                $url  = substr($buf, $offset + 19, $nl - $offset - 45);
-                $date = substr($buf, $nl - 25, 10);
+            if ($carry !== '') {
+                $nl = strpos($raw, "\n");
+                if ($nl === false) {
+                    $carry .= $raw;
+                    continue;
+                }
+                $line = $carry . substr($raw, 0, $nl);
+                $carry = '';
+                $len = strlen($line);
+                if ($len >= 45) {
+                    $url = substr($line, 19, $len - 45);
+                    $date = substr($line, $len - 25, 10);
+                    isset($visits[$url][$date]) ? $visits[$url][$date]++ : $visits[$url][$date] = 1;
+                }
+                $offset = $nl + 1;
+            }
+
+            while (($nl = strpos($raw, "\n", $offset)) !== false) {
+                $url = substr($raw, $offset + 19, $nl - $offset - 45);
+                $date = substr($raw, $nl - 25, 10);
                 isset($visits[$url][$date]) ? $visits[$url][$date]++ : $visits[$url][$date] = 1;
                 $offset = $nl + 1;
             }
+
+            $carry = $offset < $rawLen ? substr($raw, $offset) : '';
         }
 
         if ($carry !== '' && ($len = strlen($carry)) >= 45) {
-            $url  = substr($carry, 19, $len - 45);
+            $url = substr($carry, 19, $len - 45);
             $date = substr($carry, $len - 25, 10);
             isset($visits[$url][$date]) ? $visits[$url][$date]++ : $visits[$url][$date] = 1;
         }
