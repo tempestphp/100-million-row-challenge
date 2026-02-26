1b5de97c2f7b08821b914a9b633590e0df259a4c
NadavHR <nadavh2611@gmail.com>
2026-02-26 21:40:49 +0200

    first attempt

--- app/Parser.php
+++ app/Parser.php
@@ -2,12 +2,52 @@
 
 namespace App;
 
-use Exception;
-
 final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        gc_disable();
+        $csv = fopen($inputPath, 'rb');
+
+        $all = [];
+
+        while ($line = fgets($csv)) {
+            $separated = strcspn($line, ',');
+            $path = substr($line, offset: 25, length: $separated - 25);
+            $date = substr($line, offset: $separated + 1, length: 10);
+            $all[$path][$date] ??= 0;
+            $all[$path][$date]++;
+
+        }
+        fclose($csv);
+
+
+        $jsonString = "{\n";
+        $lastPath = array_key_last($all);
+        $lastDates = $all[$lastPath];
+        unset($all[$lastPath]);
+        foreach ($all as $path => $dates) {
+            $jsonString .= '    "\/blog\/'.$path.'"'.": {";
+            ksort($dates);
+            $firstDate =  key($dates);
+            $jsonString .= "\n        ".'"'.$firstDate.'": '.$dates[$firstDate];
+            unset($dates[$firstDate]);
+            foreach ($dates as $date => $count) {
+                $jsonString .= ",\n        ".'"'.$date.'": '.$count;
+            }
+
+            $jsonString .= "\n    },\n";
+        }
+        $jsonString .= '    "\/blog\/'.$lastPath.'": {';
+        ksort($lastDates);
+        $firstDate =  key($lastDates);
+        $jsonString .= "\n        ".'"'.$firstDate.'": '.$lastDates[$firstDate];
+        unset($lastDates[$firstDate]);
+        foreach ($lastDates as $date => $count) {
+            $jsonString .= ",\n        ".'"'.$date.'": '.$count;
+        }
+        $jsonString .= "\n    }\n}";
+
+        file_put_contents($outputPath, $jsonString);
     }
 }
