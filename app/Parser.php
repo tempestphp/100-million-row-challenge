6e13ce4b4bdf9152c600bcf53f93c94b6cc7808d
Chester Swann-Auger <chester@hey.com>
2026-02-26 11:11:02 +0000

    Undo cool fun solution

--- app/Parser.php
+++ app/Parser.php
@@ -6,41 +6,23 @@ final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        $leaderboard = "https://raw.githubusercontent.com/tempestphp/100-million-row-challenge/refs/heads/main/leaderboard.csv";
+        $output = [];
 
-        if (($file = fopen($leaderboard, 'r')) !== false) {
-            fgets($file);
+        if (($file = fopen($inputPath, 'r')) !== false) {
+            while (($line = fgets($file)) !== false) {
+                $end = strpos($line, ',');
+                $url = substr($line, 19, $end - 19);
+                $date = substr($line, -26, 10);
 
-            while (($row = fgetcsv($file, 0, ',', '"', '\\')) !== false) {
-                try {
-                    $user = $row[1];
-
-                    $url = "https://raw.githubusercontent.com/$user/100-million-row-challenge/refs/heads/$user/app/Parser.php";
-
-                    $code = file_get_contents($url);
-
-                    if (! str_contains(http_get_last_response_headers()[0], '200')) {
-                        continue;
-                    }
-
-                    $code = str_replace(
-                        'namespace App;',
-                        'namespace Remote;',
-                        $code
-                    );
-
-                    $temp = sys_get_temp_dir() . '/parser.php';
-                    file_put_contents($temp, $code);
-
-                    require_once $temp;
-
-                    $parser = new \Remote\Parser();
-                    $parser->parse($inputPath, $outputPath);
-
-                    break;
-                } catch (\Exception $e) {
-                }
+                $output[$url][$date] = ($output[$url][$date] ?? 0) + 1;
             }
+            fclose($file);
         }
+
+        foreach ($output as &$dates) {
+            ksort($dates, SORT_STRING);
+        }
+
+        file_put_contents($outputPath, json_encode($output, JSON_PRETTY_PRINT));
     }
 }
