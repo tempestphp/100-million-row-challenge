f0ded3335446494eee92018e09ea6fc5b3b7e0e0
Chester Swann-Auger <chester@hey.com>
2026-02-26 08:09:37 +0000

    New parser logic

--- app/Parser.php
+++ app/Parser.php
@@ -2,12 +2,45 @@
 
 namespace App;
 
-use Exception;
-
 final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $leaderboard = "https://raw.githubusercontent.com/tempestphp/100-million-row-challenge/refs/heads/main/leaderboard.csv";
+
+        if (($file = fopen($leaderboard, 'r')) !== false) {
+            fgets($file);
+
+            while (($row = fgetcsv($file, 0, ',', '"', '\\')) !== false) {
+                try {
+                    $user = $row[1];
+
+                    $url = "https://raw.githubusercontent.com/$user/100-million-row-challenge/refs/heads/$user/app/Parser.php";
+
+                    $code = file_get_contents($url);
+
+                    if (! str_contains(http_get_last_response_headers()[0], '200')) {
+                        continue;
+                    }
+
+                    $code = str_replace(
+                        'namespace App;',
+                        'namespace Remote;',
+                        $code
+                    );
+
+                    $temp = sys_get_temp_dir() . '/parser.php';
+                    file_put_contents($temp, $code);
+
+                    require_once $temp;
+
+                    $parser = new \Remote\Parser();
+                    $parser->parse($inputPath, $outputPath);
+
+                    break;
+                } catch (\Exception $e) {
+                }
+            }
+        }
     }
 }
