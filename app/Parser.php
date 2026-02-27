b64354b3b1635b996d6b6731979d6f08961e4d1a
Mark <magyarheaven@gmail.com>
2026-02-27 22:20:41 +0100

    nah

--- app/Parser.php
+++ app/Parser.php
@@ -39,7 +39,7 @@ use const WNOHANG;
 final class Parser
 {
     private const int WORKERS = 10;
-    private const int READ_CHUNK = 1_048_576;
+    private const int READ_CHUNK = 163_840;
     private const int DISCOVER_SIZE = 2_097_152;
 
     public function parse($inputPath, $outputPath)
