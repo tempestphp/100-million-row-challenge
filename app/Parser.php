8668c76a96e81dcab2f3a1abb143ae7d0c8b1bf0
Mark <magyarheaven@gmail.com>
2026-02-27 02:02:54 +0100

    wip

--- app/Parser.php
+++ app/Parser.php
@@ -40,7 +40,7 @@ use const SEEK_CUR;
 final class Parser
 {
     private const int WORKERS = 12;
-    private const int READ_CHUNK = 196_608;
+    private const int READ_CHUNK = 163_840;
     private const int DISCOVER_SIZE = 2_097_152;
 
     public function parse($inputPath, $outputPath)
