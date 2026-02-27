da6ae1410edd6dab48af389e340ba36dfa0836ff
NadavHR <nadavh2611@gmail.com>
2026-02-27 13:54:54 +0200

    trying to use the c implemented functions as much as possible

--- app/Parser.php
+++ app/Parser.php
@@ -7,47 +7,29 @@ final class Parser
     public function parse(string $inputPath, string $outputPath): void
     {
         gc_disable();
-        $csv = fopen($inputPath, 'rb');
+        $csv = fopen($inputPath, 'r');
 
         $all = [];
 
         while ($line = fgets($csv)) {
             $separated = strcspn($line, ',');
-            $path = substr($line, offset: 25, length: $separated - 25);
+            $path = substr($line, offset: 19, length: $separated - 19);
             $date = substr($line, offset: $separated + 1, length: 10);
             $all[$path][$date] ??= 0;
             $all[$path][$date]++;
 
         }
         fclose($csv);
-
-
-        $jsonString = "{\n";
-        $lastPath = array_key_last($all);
-        $lastDates = $all[$lastPath];
-        unset($all[$lastPath]);
-        foreach ($all as $path => $dates) {
-            $jsonString .= '    "\/blog\/'.$path.'"'.": {";
-            ksort($dates);
-            $firstDate =  key($dates);
-            $jsonString .= "\n        ".'"'.$firstDate.'": '.$dates[$firstDate];
-            unset($dates[$firstDate]);
-            foreach ($dates as $date => $count) {
-                $jsonString .= ",\n        ".'"'.$date.'": '.$count;
-            }
-
-            $jsonString .= "\n    },\n";
+        foreach ($all as &$dates) {
+            ksort($dates, SORT_STRING);
         }
-        $jsonString .= '    "\/blog\/'.$lastPath.'": {';
-        ksort($lastDates);
-        $firstDate =  key($lastDates);
-        $jsonString .= "\n        ".'"'.$firstDate.'": '.$lastDates[$firstDate];
-        unset($lastDates[$firstDate]);
-        foreach ($lastDates as $date => $count) {
-            $jsonString .= ",\n        ".'"'.$date.'": '.$count;
-        }
-        $jsonString .= "\n    }\n}";
 
-        file_put_contents($outputPath, $jsonString);
+        file_put_contents(
+            $outputPath,
+            json_encode(
+                $all,
+                JSON_PRETTY_PRINT
+            )
+        );
     }
 }
