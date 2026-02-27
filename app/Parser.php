dd687271b081808516c9cb1dbda91d29b09c7f73
David Pugh <plastonick@gmail.com>
2026-02-27 08:49:07 +0000

    abuse the shape of the data a bit

--- app/Parser.php
+++ app/Parser.php
@@ -2,12 +2,26 @@
 
 namespace App;
 
-use Exception;
-
 final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $handle = fopen($inputPath, 'r');
+        $outputData = [];
+        while ($row = fgets($handle)) {
+            $rowLen = strlen($row);
+
+            $site = substr($row, 19, $rowLen - 46);
+            $date = substr($row, $rowLen - 26, 10);
+
+            $outputData[$site][$date] ??= 0;
+            $outputData[$site][$date] += 1;
+        }
+
+        foreach ($outputData as &$data) {
+            ksort($data);
+        }
+
+        file_put_contents($outputPath, json_encode($outputData, JSON_PRETTY_PRINT));
     }
 }
