1a7b4ab0541549725c170c4a00fab907378c866b
Mindaugas Liubinas <mili@boozt.com>
2026-02-27 07:57:30 +0100

    Fix the code to follow the format

--- app/Parser.php
+++ app/Parser.php
@@ -9,9 +9,10 @@ final class Parser
     public function parse(string $inputPath, string $outputPath): void
     {
         $result = [];
-        $file = file($inputPath);
-        foreach ($file as $line) {
+        foreach ($this->readLine($inputPath) as $line) {
             [$url, $date] = explode(',', $line);
+            $url = str_replace('https://stitcher.io', '', $url);
+            $date = substr($date, 0, 10);
             if (!isset($result[$url])) {
                 $result[$url] = [];
             }
@@ -21,6 +22,21 @@ final class Parser
             $result[$url][$date]++;
         }
 
-        file_put_contents($outputPath, json_encode($result));
+        foreach ($result as $url => &$dates) {
+            ksort($dates);
+        }
+
+        file_put_contents($outputPath, json_encode($result, JSON_PRETTY_PRINT));
+    }
+
+    private function readLine(string $inputPath): \Generator
+    {
+        $handle = fopen($inputPath, 'r');
+        if ($handle) {
+            while (($line = fgets($handle)) !== false) {
+                yield $line;
+            }
+            fclose($handle);
+        }
     }
 }
