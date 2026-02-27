db81bd23ddb04f41a6534c5e7133b7e5c3917523
Rodrigo V. Raimundo <rodrigovalceli@gmail.com>
2026-02-27 16:44:45 -0300

    my best single threaded result so far

--- app/Parser.php
+++ app/Parser.php
@@ -2,12 +2,23 @@
 
 namespace App;
 
-use Exception;
-
 final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
-    {
-        throw new Exception('TODO');
+    {       
+        $f = fopen($inputPath, 'rnb');
+        $v = fgets($f);
+        $s = strpos($v, '/', 8);
+        $h = [];
+        do {
+            $h[substr($v, $s, -27)][] = substr($v, -26, 10);
+        } while ($v = stream_get_line($f, 8192, PHP_EOL));
+
+        foreach ($h as &$v) {
+            $v = array_count_values($v);
+            ksort($v);
+        };
+
+        file_put_contents($outputPath, json_encode($h, JSON_PRETTY_PRINT));
     }
 }
