a0cd5ba75e216abbcad86d7e16b284c6d5db7fd6
Andries Mooij <a.mooij@gmail.com>
2026-02-27 12:53:24 +0100

    clean

--- app/Parser.php
+++ app/Parser.php
@@ -6,8 +6,27 @@ use Exception;
 
 final class Parser
 {
+	private const string DOMAIN ='https://stitcher.io';
+
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+	    $fp = fopen($inputPath, 'r');
+
+	    $domainLength = strlen(self::DOMAIN);
+	    $pathVisits = [];
+
+	    while (($line = fgetcsv($fp, escape: '')) !== false) {
+		    $path = substr($line[0], $domainLength);
+		    $date = substr($line[1], 0, 10);
+
+		    $pathVisits[$path][$date] = ($pathVisits[$path][$date] ?? 0) + 1;
+	    }
+
+	    foreach ($pathVisits as $path => $dates) {
+		    ksort($pathVisits[$path]);
+	    }
+
+	    $fp = fopen($outputPath, 'w');
+	    fwrite($fp, json_encode($pathVisits, flags: JSON_PRETTY_PRINT));
     }
 }
