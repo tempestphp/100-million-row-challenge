68c7eb85a0530816b2e5e4d7e40469914e6a5699
NadavHR <nadavh2611@gmail.com>
2026-02-27 14:13:53 +0200

    numeric sort

--- app/Parser.php
+++ app/Parser.php
@@ -11,25 +11,48 @@ final class Parser
 
         $all = [];
 
-        while ($line = fgets($csv)) {
-            $separated = strcspn($line, ',');
-            $path = substr($line, offset: 19, length: $separated - 19);
-            $date = substr($line, offset: $separated + 1, length: 10);
-            $all[$path][$date] ??= 0;
-            $all[$path][$date]++;
+        $globalMinDay = PHP_INT_MAX;
+        $globalMaxDay = 0;
 
+        while (($line = fgets($csv)) !== false) {
+            $commaPos = strpos($line, ',');
+            $path = substr($line, 19, $commaPos - 19);
+            $dateOffset = $commaPos + 1;
+
+            $year  = (int)substr($line, $dateOffset, 4);
+            $month = (int)substr($line, $dateOffset + 5, 2);
+            $day   = (int)substr($line, $dateOffset + 8, 2);
+
+            $dayId = $year * 10000 + $month * 100 + $day;
+
+            $globalMinDay = min($globalMinDay, $dayId);
+            $globalMaxDay = max($globalMaxDay, $dayId);
+
+            $all[$path][$dayId] ??= 0;
+            $all[$path][$dayId]++;
         }
+
         fclose($csv);
+
         foreach ($all as &$dates) {
-            ksort($dates, SORT_STRING);
+            ksort($dates, SORT_NUMERIC);
+        }
+
+        foreach ($all as $path => &$dates) {
+            foreach ($dates as $dayId => $count) {
+                $year  = intdiv($dayId, 10000);
+                $month = intdiv($dayId % 10000, 100);
+                $day   = $dayId % 100;
+
+                $dateString = sprintf('%04d-%02d-%02d', $year, $month, $day);
+                unset($dates[$dayId]);
+                $all[$path][$dateString] = $count;
+            }
         }
 
         file_put_contents(
             $outputPath,
-            json_encode(
-                $all,
-                JSON_PRETTY_PRINT
-            )
+            json_encode($all, JSON_PRETTY_PRINT )
         );
     }
 }
