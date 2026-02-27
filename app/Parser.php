b346fd4db57abecae2221b5cfe08ef58bd7c9523
brendt <brent.roose@gmail.com>
2026-02-27 09:13:50 +0100

    wip

--- app/Parser.php
+++ app/Parser.php
@@ -8,6 +8,7 @@ final class Parser
 
     public function parse(string $inputPath, string $outputPath): void
     {
+        throw new \Exception('Nope');
         $csv = fopen($inputPath, 'r');
         $data = [];
 
