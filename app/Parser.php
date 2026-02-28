ba4d9a8101053812d80f642c8da6b049cdd636d6
Dan Scott <98423842+danscott1997@users.noreply.github.com>
2026-02-28 20:49:51 +0000

    Update Parser.php

--- app/Parser.php
+++ app/Parser.php
@@ -8,6 +8,28 @@ final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $return = [];
+
+        $handle = fopen($inputPath, "r");
+
+        while ($data = fgets($handle)) {
+            $data = explode(',', $data);
+
+            $url = $data[0];
+            $date = substr($data[1], 0, 10);
+
+            $return[$url][$date] = ($return[$url][$date] ?? 0) + 1;
+        }
+
+        fclose($handle);
+
+        foreach ($return as $_ => &$data) {
+            ksort($data);
+        }
+
+        file_put_contents(
+            $outputPath,
+            str_replace('https:\/\/stitcher.io', '', json_encode($return, JSON_PRETTY_PRINT))
+        );
     }
 }
