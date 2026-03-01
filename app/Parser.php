4065cca81cafd6c33353eb73d6ed2a498c7bfd81
David Gallagher <david@thegallagher.net>
2026-03-01 15:56:49 +1100

    Set baseline

--- app/Parser.php
+++ app/Parser.php
@@ -2,12 +2,26 @@
 
 namespace App;
 
-use Exception;
-
 final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $input = \fopen($inputPath, 'r');
+        $outputData = [];
+        while ($line = \fgets($input)) {
+            [$path, $date] = \explode(',', $line, 2);
+            $path = \substr($path, 19);
+            $date = \substr($date, 0, 10);
+            $outputData[$path][$date] ??= 0;
+            $outputData[$path][$date]++;
+        }
+        \fclose($input);
+
+        foreach ($outputData as $path => $dates) {
+            \ksort($outputData[$path]);
+        }
+
+        $outputJson = \json_encode($outputData, \JSON_PRETTY_PRINT);
+        \file_put_contents($outputPath, $outputJson);
     }
 }
