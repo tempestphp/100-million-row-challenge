07a1e9615002e31de626f7dcfbaa232be31648f8
rahulbaruah <baruah.rahul.88@gmail.com>
2026-03-01 20:03:57 +0000

    Refactor Parser class by removing unused code and comments; update .gitignore to include benchmark files; delete obsolete notes and temporary parser files

--- app/Parser.php
+++ app/Parser.php
@@ -2,25 +2,10 @@
 
 namespace App;
 
-use Exception;
-
 final class Parser
 {
-    // private const PREFIX = 'https://stitcher.io';
-    // private const PREFIX_LEN = 19;
-    // private const TIMESTAMP_LEN = 10;
-
     public function parse(string $inputPath, string $outputPath): void
     {
-        // throw new Exception('TODO');
-
-        // $inputPath = __DIR__.'/../data/test-data.csv';
-        // $outputPath = __DIR__.'/../data/test-data.json';
-
-        // $file = new \SplFileObject($inputPath);
-        // $file->setFlags(\SplFileObject::READ_CSV);
-        // $file->setCsvControl(',', '"', '\\');
-
         $handle = fopen($inputPath, 'r');
 
         $outArray = [];
@@ -34,72 +19,17 @@ final class Parser
             $url = substr($line, 19, $commaPos - 19);
             $timestamp = substr($line, $commaPos + 1, 10);
 
-            // $row = explode(',', trim($row));
-            // if (!isset($row[0], $row[1])) {
-            //     break;
-            // }
-
-            // $url = json_encode((string) $row[0]);
-            // $url = str_replace('/', '\/', str_replace('https://stitcher.io', '', $row[0]));
-            // $url = str_replace('https://stitcher.io', '', $row[0]);
-            // $url = substr($rawUrl, 19);
-            // $timestamp = substr($row[1], 0, 10);
-
-            // $timestampCount = $outArray[$url][$timestamp] ?? 0;
-            // ++$timestampCount;
-
             if (isset($outArray[$url][$timestamp])) {
                 ++$outArray[$url][$timestamp];
             } else {
                 $outArray[$url][$timestamp] = 1;
             }
-
-            // if (false !== $tempStrSearch) {
-            //     print_r($outArray[$url]);
-            //     echo "\n";
-            // }
         }
 
         fclose($handle);
-
-        // $first_N_elements = array_slice($outArray, 0, 3, true);
-
-        // print_r($outArray);
-
-        // echo "\n";
-
-        // $chunks = array_chunk($outArray, 10, true);
-
-        // file_put_contents($outputPath, "{\n", FILE_APPEND | LOCK_EX);
-        // foreach ($chunks as $chunk) {
-        //     file_put_contents(
-        //         $outputPath,
-        //         trim(json_encode($chunk, JSON_PRETTY_PRINT), '{}'),
-        //         FILE_APPEND | LOCK_EX
-        //     );
-        // }
-        // file_put_contents($outputPath, "\n}", FILE_APPEND | LOCK_EX);
-
-        // The FILE_APPEND flag preserves existing content and adds new data to the end
         file_put_contents(
             $outputPath,
             str_replace([' ', "\n"], '', json_encode($outArray))
         );
     }
-
-    /* public function readFileLineByLine(string $filePath): \Generator
-    {
-        $handle = fopen($filePath, 'r');
-        if (!$handle) {
-            throw new \Exception("Unable to open the file: {$filePath}");
-        }
-
-        try {
-            while (($line = fgets($handle)) !== false) {
-                yield $line; // Yields one line at a time
-            }
-        } finally {
-            fclose($handle); // Ensures the file is closed even if an exception occurs
-        }
-    } */
 }
