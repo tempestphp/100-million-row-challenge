330aa096233ecae8e371a92fc590bc10724818b7
N-Silbernagel <6422477+N-Silbernagel@users.noreply.github.com>
2026-03-01 01:20:12 +0100

    Use dateIds
    0.62 -> 0.57

--- app/Parser.php
+++ app/Parser.php
@@ -11,24 +11,7 @@ final class Parser
     {
         gc_disable();
 
-        $outputData = [];
-
-        $input = fopen($inputPath, 'r');
-        stream_set_read_buffer($input, 4 * 2 ^ 10 * 2 ^ 10);
-
-        while ($line = fgets($input)) {
-            $commaPos = strpos($line, ',');
-
-            $path = substr($line, 19, $commaPos - 19);
-
-            $date = substr($line, $commaPos + 1, 10);
-
-            $outputData[$path] ??= [];
-
-            $outputData[$path][$date] ??= 0;
-            $outputData[$path][$date]++;
-        }
-
+        $dateIds = [];
         $dates = [];
         $dateCount = 0;
         for ($y = 21; $y <= 26; $y++) {
@@ -42,10 +25,31 @@ final class Parser
                 $ymStr = "20{$y}-{$mStr}-";
                 for ($d = 1; $d <= $maxD; $d++) {
                     $key = $ymStr . (($d < 10 ? '0' : '') . $d);
+                    $dateIds[$key] = $dateCount;
                     $dates[$dateCount] = $key;
                     $dateCount++;
                 }
             }
+        }
+
+        $outputData = [];
+
+        $input = fopen($inputPath, 'r');
+        stream_set_read_buffer($input, 4_194_304);
+
+        while ($line = fgets($input)) {
+            $commaPos = strpos($line, ',');
+
+            $path = substr($line, 19, $commaPos - 19);
+
+            $date = substr($line, $commaPos + 1, 10);
+
+            $outputData[$path] ??= [];
+
+            $dateId = $dateIds[$date];
+
+            $outputData[$path][$dateId] ??= 0;
+            $outputData[$path][$dateId]++;
         }
 
         $output = fopen('php://memory', 'w');
@@ -54,18 +58,20 @@ final class Parser
 
         $totalPathsCount = count($outputData);
         $pathIndex = 0;
-        foreach ($outputData as $path => &$pathCounts) {
+        foreach ($outputData as $path => $pathCounts) {
             $escapedPath = str_replace('/', '\/', $path);
             fwrite($output, "    \"$escapedPath\": {" . PHP_EOL);
 
             $totalDatesCount = count($pathCounts);
             $dateIndex = 0;
 
-            foreach ($dates as $date) {
-                $count = $pathCounts[$date] ?? null;
+            foreach ($dateIds as $dateId) {
+                $count = $pathCounts[$dateId] ?? null;
                 if ($count === null) {
                     continue;
                 }
+
+                $date = $dates[$dateId];
 
                 fwrite($output, "        \"$date\": $count");
                 if ($dateIndex < $totalDatesCount - 1) {
