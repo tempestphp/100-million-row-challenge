5e94e98c475e6738b71703802872faa5b48105ae
Mark <magyarheaven@gmail.com>
2026-03-01 15:16:57 +0100

    10x unroll

--- app/Parser.php
+++ app/Parser.php
@@ -188,9 +188,17 @@ final class Parser
             }
 
             $p = 25;
-            $fence = $lastNl - 960;
+            $fence = $lastNl - 1200;
 
             while ($p < $fence) {
+                $sep = strpos($chunk, ',', $p);
+                $buckets[$pathIds[substr($chunk, $p, $sep - $p)]] .= $dateIdChars[substr($chunk, $sep + 3, 8)];
+                $p = $sep + 52;
+
+                $sep = strpos($chunk, ',', $p);
+                $buckets[$pathIds[substr($chunk, $p, $sep - $p)]] .= $dateIdChars[substr($chunk, $sep + 3, 8)];
+                $p = $sep + 52;
+
                 $sep = strpos($chunk, ',', $p);
                 $buckets[$pathIds[substr($chunk, $p, $sep - $p)]] .= $dateIdChars[substr($chunk, $sep + 3, 8)];
                 $p = $sep + 52;
