f8625e2af05bb77caa3241d0c39fd257d471e17f
Nazlican <nazlican@trone.nl>
2026-03-02 13:59:23 +0100

    Remove validation overhead in hot loop, replace constant with literal (19) to eliminate lookups, use isset() + ++ for efficient counting and remove count() check before ksort()

--- app/Parser.php
+++ app/Parser.php
@@ -4,51 +4,27 @@ namespace App;
 
 final class Parser
 {
-    private const PATH_START = 19;
-
     public function parse(string $inputPath, string $outputPath): void
     {
         $handle = fopen($inputPath, 'r');
-        if ($handle === false) {
-            return;
-        }
-
         $visits = [];
-
-        
+    
         while (($line = fgets($handle)) !== false) {
             $separatorPosition = strrpos($line, ',');
-            if ($separatorPosition === false || $separatorPosition < self::PATH_START) {
-                continue;
-            }
-
-            $path = substr($line, self::PATH_START, $separatorPosition - self::PATH_START);
-
+            $path = substr($line, 19, $separatorPosition - 19);
             $date = substr($line, $separatorPosition + 1, 10);
-            $visits[$path][$date] = ($visits[$path][$date] ?? 0) + 1;
+            
+            if (isset($visits[$path][$date])) {
+                $visits[$path][$date]++;
+            } else {
+                $visits[$path][$date] = 1;
+            }
         }
-
 
         foreach ($visits as &$dailyVisits) {
-            if (count($dailyVisits) > 1) {
-                ksort($dailyVisits);
-            }
+            ksort($dailyVisits);
         }
-        unset($dailyVisits);
 
-
-        // Only for Windows
-        // $json = json_encode($visits, JSON_PRETTY_PRINT);
-        //
-        // if ($json === false) {
-        //     return;
-        // }
-        //
-        // if (PHP_EOL !== "\n") {
-        //     $json = str_replace("\n", PHP_EOL, $json);
-        // }
-        //
-        // file_put_contents($outputPath, $json);
         file_put_contents($outputPath, json_encode($visits, JSON_PRETTY_PRINT));
     }
 }
