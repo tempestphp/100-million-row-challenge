ac138c42d98d355471bf221ebb37dc1b727b2d49
Chester Swann-Auger <chester@hey.com>
2026-03-02 16:55:24 +0000

    Change keys to be numeric

--- app/Parser.php
+++ app/Parser.php
@@ -7,20 +7,32 @@ final class Parser
     public function parse(string $inputPath, string $outputPath): void
     {
         $output = [];
+        $file = fopen($inputPath, 'r');
 
-        if (($file = fopen($inputPath, 'r')) !== false) {
-            while (($line = fgets($file)) !== false) {
-                $end = strpos($line, ',');
-                $url = substr($line, 19, $end - 19);
-                $date = substr($line, -26, 10);
+        while (($line = fgets($file)) !== false) {
+            $url = substr($line, 19, -27);
+            $date = substr($line, -26, 10);
+            $date = (int) str_replace('-', '', $date);
 
-                $output[$url][$date] = ($output[$url][$date] ?? 0) + 1;
+            if (isset($output[$url][$date])) {
+                $output[$url][$date]++;
+            } else {
+                $output[$url][$date] = 1;
             }
-            fclose($file);
         }
 
         foreach ($output as &$dates) {
-            ksort($dates, SORT_STRING);
+            ksort($dates, SORT_NUMERIC);
+
+            $new = [];
+            foreach ($dates as $key => $value) {
+                $year = intdiv($key, 10000);
+                $month = intdiv($key, 100) % 100;
+                $day = $key % 100;
+
+                $new[sprintf('%04d-%02d-%02d', $year, $month, $day)] = $value;
+            }
+            $dates = $new;
         }
 
         file_put_contents($outputPath, json_encode($output, JSON_PRETTY_PRINT));
