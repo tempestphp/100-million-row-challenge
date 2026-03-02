e49c1ef2df48fcccdef11d88578fbb202343be73
David Gallagher <david@thegallagher.net>
2026-03-02 13:03:19 +1000

    Flat array with perfect hash

--- app/Parser.php
+++ app/Parser.php
@@ -4,24 +4,67 @@ namespace App;
 
 final class Parser
 {
+    private const int URL_COUNT = 268;
+    private const int URL_BITS = 9;
+    private const int YEAR_BITS = 3;
+    private const int MONTH_BITS = 4;
+    private const int DAY_BITS = 5;
+    private const int DAY_MONTH_BITS = self::MONTH_BITS + self::DAY_BITS;
+    private const int DATE_BITS = self::YEAR_BITS + self::DAY_MONTH_BITS;
+    private const int HASH_BITS = self::URL_BITS + self::DATE_BITS;
+    private const int HASH_SIZE = 2 ** self::HASH_BITS;
+
     public function parse(string $inputPath, string $outputPath): void
     {
-        $input = \fopen($inputPath, 'r');
-        $outputData = [];
-        while ($line = \fgets($input)) {
-            [$path, $date] = \explode(',', $line, 2);
-            $path = \substr($path, 19);
-            $date = \substr($date, 0, 10);
-            $outputData[$path][$date] ??= 0;
-            $outputData[$path][$date]++;
-        }
-        \fclose($input);
+        $outputData = \array_fill(0, self::HASH_SIZE, 0);
 
-        foreach ($outputData as $path => $dates) {
-            \ksort($outputData[$path]);
+        $inputStream = \fopen($inputPath, 'r');
+        $urlCount = 0;
+        $urlMap = [];
+
+        while ($urlCount < self::URL_COUNT && $line = \fgets($inputStream)) {
+            $path = \substr($line, 25, -27);
+            $urlMap[$path] ??= $urlCount++ << self::DATE_BITS;
+            $y = ((int) \substr($line, -23, 1)) << self::DAY_MONTH_BITS;
+            $m = ((int) \substr($line, -21, 2)) << self::DAY_BITS;
+            $d = ((int) \substr($line, -18, 2));
+            $hash = $urlMap[$path] | $y | $m | $d;
+            $outputData[$hash]++;
         }
 
-        $outputJson = \json_encode($outputData, \JSON_PRETTY_PRINT);
-        \file_put_contents($outputPath, $outputJson);
+        while ($line = \fgets($inputStream)) {
+            $path = \substr($line, 25, -27);
+            $y = ((int) \substr($line, -26, 1)) << self::DAY_MONTH_BITS;
+            $m = ((int) \substr($line, -21, 2)) << self::DAY_BITS;
+            $d = ((int) \substr($line, -18, 2));
+            $hash = $urlMap[$path] | $y | $m | $d;
+            $outputData[$hash]++;
+        }
+
+        \fclose($inputStream);
+
+        $outputStream = \fopen($outputPath, 'w');
+        \fwrite($outputStream, "{\n");
+
+        foreach ($urlMap as $url => $urlHash) {
+            \fwrite($outputStream, "    \"\/blog\/{$url}\": {\n");
+
+            $end = $urlHash + (2 ** self::DATE_BITS);
+            for ($i = $urlHash; $i < $end; $i++) {
+                if ($outputData[$i] > 0) {
+                    $y = ($i >> self::DAY_MONTH_BITS) & 0b111;
+                    $m = ($i >> self::DAY_BITS) & 0b1111;
+                    $d = $i & 0b11111;
+                    \fwrite($outputStream, \sprintf('        "202%d-%02d-%02d": %d,', $y, $m, $d, $outputData[$i]) . "\n");
+                }
+            }
+
+            \fseek($outputStream, -2, \SEEK_CUR);
+            \fwrite($outputStream, "\n    },\n");
+        }
+
+        \fseek($outputStream, -2, \SEEK_CUR);
+        \fwrite($outputStream, "\n}");
+        \fclose($outputStream);
     }
 }
