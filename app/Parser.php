0394547887e0aeb9b291807a49a9dfc9dc3f72ac
Matt Raines <matt@praterraines.co.uk>
2026-03-02 17:33:51 +0000

    Simple solution largely using built-in functions

--- app/Parser.php
+++ app/Parser.php
@@ -2,12 +2,31 @@
 
 namespace App;
 
-use Exception;
+use App\Commands\Visit;
 
 final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $max_uri_length = Visit::all()
+            |> (fn($vs) => array_map(fn($v) => strlen($v->uri) - 17, $vs))
+            |> max(...);
+
+        $uris = [];
+
+        $in = fopen($inputPath, 'r');
+        fseek($in, 19); // Ignore https://stitcher.io
+        while ($uri = stream_get_line($in, $max_uri_length, ",")) {
+            $date = stream_get_line($in, 10);
+            fseek($in, 35, SEEK_CUR); // Skip time and https://stitcher.io
+            $uris[$uri][] = $date; // It's cheaper not to check for and initialise the array
+        }
+
+        foreach ($uris as &$visits) {
+            $visits = array_count_values($visits);
+            ksort($visits, SORT_STRING);
+        }
+
+        file_put_contents($outputPath, json_encode($uris, JSON_PRETTY_PRINT, 3));
     }
 }
