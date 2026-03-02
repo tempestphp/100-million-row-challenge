a350034dba3c77dd00ac5b3204661c1ce69a032d
Mark <magyarheaven@gmail.com>
2026-03-02 16:32:24 +0100

    out of ideas - hardpush to "best ever" if things go south

--- app/Parser.php
+++ app/Parser.php
@@ -188,9 +188,17 @@ final class Parser
             }
 
             $p = 25;
-            $fence = $lastNl - 606;
+            $fence = $lastNl - 808;
 
             while ($p < $fence) {
+                $sep = strpos($chunk, ',', $p);
+                $buckets[$pathIds[substr($chunk, $p, $sep - $p)]] .= $dateIdChars[substr($chunk, $sep + 3, 8)];
+                $p = $sep + 52;
+
+                $sep = strpos($chunk, ',', $p);
+                $buckets[$pathIds[substr($chunk, $p, $sep - $p)]] .= $dateIdChars[substr($chunk, $sep + 3, 8)];
+                $p = $sep + 52;
+
                 $sep = strpos($chunk, ',', $p);
                 $buckets[$pathIds[substr($chunk, $p, $sep - $p)]] .= $dateIdChars[substr($chunk, $sep + 3, 8)];
                 $p = $sep + 52;
