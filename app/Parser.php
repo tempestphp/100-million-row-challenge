022552bfce770c1da4c0aeffbe8bad2ec46f8c0e
Mindaugas Liubinas <mili@boozt.com>
2026-03-03 16:18:34 +0200

    Do not store boundaries

--- app/Parser.php
+++ app/Parser.php
@@ -2,8 +2,6 @@
 
 namespace App;
 
-use SplFixedArray;
-
 final class Parser
 {
     private const int SLUG_OFFSET = 25;
@@ -15,45 +13,25 @@ final class Parser
 
     public function parse(string $inputPath, string $outputPath): void
     {
-        $boundaries = $this->getChunkBoundaries($inputPath, filesize($inputPath));
-        $totalChunks = $boundaries->count() - 1;
-        $currentChunk = 0;
-        while ($currentChunk < $totalChunks) {
-            if (null === $boundaries[$currentChunk + 1]) {
-                break;
-            }
-            $this->processChunk(
-                $inputPath,
-                $boundaries[$currentChunk],
-                $boundaries[++$currentChunk]
-            );
-        }
-
-        $this->writeOutput($outputPath);
-    }
-
-    private function getChunkBoundaries(string $inputPath, int $fileSize): SplFixedArray
-    {
+        $fileSize = filesize($inputPath);
         $handle = fopen($inputPath, 'r');
-        $boundaries = new SplFixedArray(ceil($fileSize / self::CHUNK_TARGET_SIZE) + 1);
-        $boundaries[0] = 0;
-        $boundaryIndex = 1;
-        $nextPos = 0;
+        $start = 0;
         do {
-            $nextPos = min($nextPos + self::CHUNK_TARGET_SIZE, $fileSize);
-            fseek($handle, $nextPos);
+            $stop = min($start + self::CHUNK_TARGET_SIZE, $fileSize);
+            fseek($handle, $stop);
             $line = fgets($handle);
             if (false === $line) {
-                $boundaries[$boundaryIndex] = $fileSize;
-                break;
+                $stop = $fileSize;
+            } else {
+                $stop += strlen($line);
             }
-            $nextPos += strlen($line);
-            $boundaries[$boundaryIndex++] = $nextPos;
-        } while ($fileSize > $nextPos);
+            $this->processChunk($inputPath, $start, $stop);
+            $start = $stop;
+        } while ($fileSize > $stop);
 
         fclose($handle);
 
-        return $boundaries;
+        $this->writeOutput($outputPath);
     }
 
     private function processChunk(string $inputPath, int $start, int $end): void
@@ -65,7 +43,6 @@ final class Parser
             $commaPosition = strpos($buffer, ',', $slugStart);
             $slug = substr($buffer, $slugStart, $commaPosition - $slugStart);
             $date = substr($buffer, $commaPosition + 1, self::DATE_LENGTH);
-
             $this->result[$slug][$date] = ($this->result[$slug][$date] ?? 0) + 1;
             $slugStart = $commaPosition + self::COMMA_TO_NEXT_SLUG;
         } while ($slugStart <= $contentLength);
