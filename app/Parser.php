f1617e7ed7b7aa5d2945d18f54f63169b91691c7
gordonmcvey <grj.mcvey@gmail.com>
2026-03-06 09:32:15 +0000

    Pre-compute dates to reduce memory allocation time overhead

--- app/Parser.php
+++ app/Parser.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace App;
 
-//use App\Commands\Visit;
+use DateTime;
 use Exception;
 use Generator;
+use function array_filter;
 use function count;
 use function fclose;
 use function feof;
@@ -17,6 +18,7 @@ use function fread;
 use function gc_disable;
 use function json_encode;
 use function preg_match_all;
+use function sprintf;
 use function str_ends_with;
 
 final class Parser
@@ -24,6 +26,8 @@ final class Parser
     private const int READ_CHUNK_SIZE = 1024 * 1024;
 
     private const string REGEX_LINE_PARSER = ";^https://stitcher.io(/[^,]+),(.{10});m";
+
+    private const int START_YEAR = 1965;
 
     /**
      * @var false|resource
@@ -38,6 +42,10 @@ final class Parser
         gc_disable();
         $toEncode = [];
 
+        // Build a pre-computed date array, and then copy it for every new path we encounter.  This means 1 date-related
+        // memory allocation per new path instead of one per new path/date combination
+        $preComputedDates = $this->prebuildDates();
+
         try {
             ($this->inputHandle = fopen($inputPath, "r")) || throw new Exception("Couldn't open input file");
             foreach ($this->getChunk() as $chunk) {
@@ -48,11 +56,15 @@ final class Parser
                 for ($key = 0; $key < $matchCount; $key++) {
                     $path = $matches[1][$key];
                     $date = $matches[2][$key];
-                    $toEncode[$path][$date] = ($toEncode[$path][$date] ?? 0) + 1;
+
+                    isset($toEncode[$path]) || $toEncode[$path] = $preComputedDates;
+
+                    $toEncode[$path][$date] += 1;
                 }
             }
 
             foreach($toEncode as &$dates) {
+                $dates = array_filter($dates);
                 ksort($dates);
             }
 
@@ -74,5 +86,22 @@ final class Parser
             }
             yield($str);
         }
+    }
+
+    private function prebuildDates(): array
+    {
+        $dates = [];
+        $now = new DateTime();
+        $currentYear = (int) $now->format("Y");
+
+        for ($y = self::START_YEAR; $y <= $currentYear; $y++) {
+            for ($m = 1; $m <= 12; $m++) {
+                for ($d = 1; $d <= 31; $d++) {
+                    $dates[sprintf("%04d-%02d-%02d", $y, $m, $d)] = 0;
+                }
+            }
+        }
+
+        return $dates;
     }
 }
