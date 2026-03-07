1e9a88cd921b943f83896d5a89a526ad3882069a
Wouter <wouter@wouterkelderman.nl>
2026-03-07 22:21:03 +0100

    first try :)

--- app/Parser.php
+++ app/Parser.php
@@ -8,6 +8,27 @@ final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $stream = fopen($inputPath, 'r');
+
+        while($line = fgets($stream)) {
+            $string = substr($line, 19);
+            $array = explode(',', $string);
+            $uri = $array[0];
+            $date = substr($array[1], 0, 10);
+
+            if (!isset($result[$uri][$date])) {
+                $result[$uri][$date] = 0;
+            }
+
+            $result[$uri][$date]++;
+        }
+
+        foreach ($result as &$dates) {
+            ksort($dates);
+        }
+
+        $json = json_encode($result, flags: \JSON_PRETTY_PRINT);
+
+        file_put_contents($outputPath, $json);
     }
 }
