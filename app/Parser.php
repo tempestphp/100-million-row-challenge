14edda8c7514660d1b7290e63acc98eaab2e7dde
boloto1979 <pedro.lima1979@hotmail.com>
2026-03-09 17:29:49 -0300

    boloto1979

--- app/Parser.php
+++ app/Parser.php
@@ -8,6 +8,39 @@ final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $handle = fopen($inputPath, 'rb');
+        
+        stream_set_chunk_size($handle, 128 * 1024);
+
+        $counts = [];
+
+        while (($line = fgets($handle)) !== false) {
+            $commaPos = strrpos($line, ',');
+            if ($commaPos === false) continue;
+
+            $slashPos = strpos($line, '/', 8);
+            if ($slashPos !== false && $slashPos < $commaPos) {
+                $path = substr($line, $slashPos, $commaPos - $slashPos);
+            } else {
+                $path = '/';
+            }
+
+            $date = substr($line, $commaPos + 1, 10);
+
+            if (isset($counts[$path][$date])) {
+                ++$counts[$path][$date];
+            } else {
+                $counts[$path][$date] = 1;
+            }
+        }
+        
+        fclose($handle);
+
+        foreach ($counts as &$dates) {
+            ksort($dates);
+        }
+        unset($dates);
+
+        file_put_contents($outputPath, json_encode($counts, JSON_PRETTY_PRINT));
     }
 }
