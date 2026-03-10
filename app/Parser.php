ac7448aad2b151f584a6ac9a5af0e193210bca5a
boloto1979 <pedro.lima1979@hotmail.com>
2026-03-09 21:17:59 -0300

    boloto1979

--- app/Parser.php
+++ app/Parser.php
@@ -8,11 +8,15 @@ final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
+        gc_disable();
         $handle = fopen($inputPath, 'rb');
         
         stream_set_chunk_size($handle, 128 * 1024);
 
         $counts = [];
+        $urlMap = [];
+        $urls = [];
+        $nextUrlId = 0;
 
         while (($line = fgets($handle)) !== false) {
             $commaPos = strrpos($line, ',');
@@ -25,22 +29,32 @@ final class Parser
                 $path = '/';
             }
 
+            if (isset($urlMap[$path])) {
+                $id = $urlMap[$path];
+            } else {
+                $id = $nextUrlId++;
+                $urlMap[$path] = $id;
+                $urls[$id] = $path;
+                $counts[$id] = [];
+            }
+
             $date = substr($line, $commaPos + 1, 10);
 
-            if (isset($counts[$path][$date])) {
-                ++$counts[$path][$date];
+            if (isset($counts[$id][$date])) {
+                ++$counts[$id][$date];
             } else {
-                $counts[$path][$date] = 1;
+                $counts[$id][$date] = 1;
             }
         }
         
         fclose($handle);
 
-        foreach ($counts as &$dates) {
+        $output = [];
+        foreach ($counts as $id => $dates) {
             ksort($dates);
+            $output[$urls[$id]] = $dates;
         }
-        unset($dates);
 
-        file_put_contents($outputPath, json_encode($counts, JSON_PRETTY_PRINT));
+        file_put_contents($outputPath, json_encode($output, JSON_PRETTY_PRINT));
     }
 }
