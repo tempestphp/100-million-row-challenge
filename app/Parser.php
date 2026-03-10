d7155fd91623be99661864b7c242fed3804da345
prateekbhujel <prateekbhujelpb@gmail.com>
2026-03-10 15:20:51 +0545

    Increase worker count

--- app/Parser.php
+++ app/Parser.php
@@ -27,7 +27,7 @@ use function substr;
 
 final class Parser
 {
-    private const int WORKERS = 8;
+    private const int WORKERS = 10;
     private const int DISCOVERY_BYTES = 262144;
     private const int READ_CHUNK = 393216;
     private const int MIN_SLUG_LEN = 4;
