4d8d68b09236a7eea8dedc9367af20c6959df283
Andries Mooij <a.mooij@gmail.com>
2026-03-10 13:34:07 +0100

    better

--- app/Parser.php
+++ app/Parser.php
@@ -3,96 +3,94 @@
 namespace App;
 
 use Exception;
+use App\Commands\Visit;
 
 final class Parser
 {
 	private const string DOMAIN ='https://stitcher.io/blog/';
-	private const int PROCESSES = 4;
-	private const int READ_BUFFER_SIZE = 8 * 1024 * 1024;
+	// private const int PROCESSES = 4;
+	private const int READ_BUFFER_SIZE = 16 * 1024 * 1024;
 	private const int WRITE_BUFFER_SIZE = 8 * 1024 * 1024;
 
 	public function parse(string $inputPath, string $outputPath): void
 	{
-	    $fp = \fopen($inputPath, 'r');
+		$domainLength = \strlen(self::DOMAIN);
 
-	    $domainLength = \strlen(self::DOMAIN);
-	    $pathVisits = [];
-	    $pathHash = [];
+		$pathIndex = [];
+		$idx = 0;
+		foreach (Visit::all() as $visit) {
+			$pathIndex[substr($visit->uri, $domainLength)] = $idx++;
+		}
+		$pathIndexReverse = array_flip($pathIndex);
 
-	    $t = microtime(true);
+		$dateIndex = [];
+		$dateNo = 0;
+		for ($y = 21; $y <= 26; $y++) {
+			for ($m = 1; $m <= 12; $m++) {
+				$days = match($m) {
+					2 => $y === 24 ? 29 : 28,
+					4, 6, 9, 11 => 30,
+					default => 31,
+				};
+				$ym = $y . '-' . ($m < 10 ? '0' : '') . $m . '-';
+				for ($d = 1; $d <= $days; $d++) {
+					$dateIndex[$ym . ($d < 10 ? '0' : '') . $d] = $dateNo++;
+				}
+			}
+		}
+		$dateIndexReverse = array_flip($dateIndex);
 
-	    \stream_set_read_buffer($fp, 0);
+		$fp = \fopen($inputPath, 'r');
 
-	    $remainder = '';
-	    $chunkNo = 0;
-	    $hashPath = [];
-	    while (($chunk = \fread($fp, self::READ_BUFFER_SIZE)) !== '') {
-		    if ($remainder !== '') {
-			    $chunk = $remainder . $chunk;
-		    }
+		$pathVisits = [];
 
-		    $prevNewLine = -1;
-		    $newLine = \strpos($chunk, "\n", 0);
-		    try {
-			    do {
-				    $path = \substr($chunk, $prevNewLine + 1 + $domainLength, ($newLine - 27) - $prevNewLine - $domainLength);
-				    $hash = $hashPath[$path] ?? \hash('xxh32', $path, true);
+		$t = microtime(true);
 
-				    $y = ((int) $chunk[$newLine - 23]) * 10 + (int) $chunk[$newLine - 22];
-				    $m = ((int) $chunk[$newLine - 20]) * 10 + (int) $chunk[$newLine - 19];
-				    $d = ((int) $chunk[$newLine - 17]) * 10 + (int) $chunk[$newLine - 16];
+		\stream_set_read_buffer($fp, 0);
 
-				    $time = ($y << 9) | ($m << 5) | $d;
+		$remainder = '';
+		restore_error_handler();
+		while (($chunk = \fread($fp, self::READ_BUFFER_SIZE)) !== '') {
+			$prevNewLine = -1;
+			$newLine = \strpos($chunk, "\n", 0);
+			try {
+				do {
+					$pathIdx = $pathIndex[\substr($chunk, $prevNewLine + 1 + $domainLength, ($newLine - 27) - $prevNewLine - $domainLength)];
+					$dateIdx = $dateIndex[\substr($chunk, $newLine - 23, 8)];
 
-				    if (!isset($pathHash[$hash])) {
-					    $pathHash[$hash] = $path;
-					    $hashPath[$path] = $hash;
-					    $pathVisits[$hash][$time] = 0;
-				    }
+					$pathVisits[$pathIdx][$dateIdx]= ($pathVisits[$pathIdx][$dateIdx] ?? 0) + 1;
 
-				    if (isset($pathVisits[$hash][$time])) {
-					    $pathVisits[$hash][$time]++;
-				    } else {
-					    $pathVisits[$hash][$time] = 1;
-				    }
+					$prevNewLine = $newLine;
+					$newLine = \strpos($chunk, "\n", $newLine + 1);
+				} while ($newLine !== false);
+			} catch (\ValueError) {
+				$newLine = false;
+			}
 
-				    $prevNewLine = $newLine;
-				    $newLine = \strpos($chunk, "\n", $newLine + 1);
-			    } while ($newLine !== false);
-		    } catch (\ValueError) {
-			    $newLine = false;
-		    }
+			\fseek($fp, ($prevNewLine + 1 - strlen($chunk)), SEEK_CUR);
+		}
 
-		    $remainder = \substr($chunk, $prevNewLine + 1);
-		    $chunkNo++;
-	    }
+		$fp = \fopen($outputPath, 'w');
+		\stream_set_write_buffer($fp, self::WRITE_BUFFER_SIZE);
+		\fwrite($fp, "{\n");
 
-	    $fp = \fopen($outputPath, 'w');
-	    \stream_set_write_buffer($fp, self::WRITE_BUFFER_SIZE);
-	    \fwrite($fp, "{\n");
-	    foreach ($pathVisits as $hash => $dates) {
-		    $line = "    \"\\/blog\\/" . str_replace('/', '\\/', $pathHash[$hash]) . "\": {\n";
-		    ksort($dates);
+		foreach ($pathVisits as $pathIdx => $dates) {
+			$line = "    \"\\/blog\\/" . str_replace('/', '\\/', $pathIndexReverse[$pathIdx]) . "\": {\n";
+			ksort($dates);
 
-		    foreach ($dates as $time => $count) {
-			    $y = $time >> 9;
-			    $m = ($time >> 5) & 15;
-			    $m = $m < 10 ? ('0' . $m) : $m;
-			    $d = $time & 31;
-			    $d = $d < 10 ? ('0' . $d) : $d;
+			foreach ($dates as $dateNo => $count) {
+				$line .= "        \"20" . $dateIndexReverse[$dateNo] . "\": " . $count . ",\n";
+			}
+			$line[-2] = "\n";
+			$line[-1] = " ";
+			$line .= "   },\n";
 
-			    $line .= "        \"20" . $y . "-" . $m . "-" . $d . "\": " . $count . ",\n";
-		    }
-		    $line[-2] = "\n";
-		    $line[-1] = " ";
-		    $line .= "   },\n";
+			\fwrite($fp, $line);
+		}
 
-		    \fwrite($fp, $line);
-	    }
-
-	    \fseek($fp, -2, SEEK_CUR);
-	    \fwrite($fp, "\n}");
+		\fseek($fp, -2, SEEK_CUR);
+		\fwrite($fp, "\n}");
 
 
-    }
+	}
 }
