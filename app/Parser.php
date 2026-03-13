856b9fac6c9a06fc4701e89d8ba9acba34eafaa9
David Fernández <david@mundosaparte.com>
2026-03-13 08:33:04 +0100

    https://github.com/driade/100-million-row-challenge

--- app/Parser.php
+++ app/Parser.php
@@ -1,13 +1,22 @@
 <?php
-
 namespace App;
-
-use Exception;
 
 final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $fp = fopen($inputPath, 'r');
+        while (! feof($fp)) {
+            $line = fgets($fp);
+            $key  = substr($line, 19, strpos($line, ',') - 19);
+            if ($key !== '') {
+                $results[$key][] = substr($line, strpos($line, ',') + 1, 10);
+            }
+        }
+        foreach ($results as $key => $values) {
+            sort($values);
+            $results[$key] = array_count_values($values);
+        }
+        file_put_contents($outputPath, json_encode($results, JSON_PRETTY_PRINT));
     }
 }
