707477587c7f8691f63f797bd78aee8b17adc484
Paul Taylor <pltorigami@gmail.com>
2026-03-13 16:38:46 -0400

    First naive attempt

--- app/Parser.php
+++ app/Parser.php
@@ -2,12 +2,32 @@
 
 namespace App;
 
-use Exception;
-
 final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $results = [];
+        $resource = \fopen($inputPath, 'r');
+
+        while (($line = \fgetcsv($resource, escape: '\\')) !== FALSE) {
+            $path = \str_replace('https://stitcher.io', '', $line[0]);
+            $date = \substr($line[1], 0, 10);
+            if (!isset($results[$path])) {
+                $results[$path] = [$date => 1];
+            } else {
+                if (!isset($results[$path][$date])) {
+                    $results[$path][$date] = 1;
+                } else {
+                    $results[$path][$date] += 1;
+                }
+            }
+        }
+        \fclose($resource);
+
+        foreach ($results as $path => &$path_arr) {
+            \ksort($path_arr);
+        }
+
+        \file_put_contents($outputPath, \json_encode($results, \JSON_PRETTY_PRINT));
     }
 }
