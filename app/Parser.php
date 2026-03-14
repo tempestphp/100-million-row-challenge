7d1ed98a9bb2d0f361b527964717c54c1e5dbba3
RuggMatt <8870594+RuggMatt@users.noreply.github.com>
2026-03-14 01:21:00 +0000

    inital attempt

--- app/Parser.php
+++ app/Parser.php
@@ -2,12 +2,48 @@
 
 namespace App;
 
-use Exception;
+use Throwable;
+
+use function Tempest\Support\Str\slice;
 
 final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $csvFile = $inputPath;
+        $file = fopen($csvFile, "r");
+        $arr = [];
+        try {
+            while ($row = fgetcsv($file, escape: "\\")) {
+                [$dest, $time] = $row;
+                $dest = slice($dest, 19);
+                $arr[$dest] ??= [];
+                $arr[$dest][$k = slice($time, 0, 10)] ??= 0;
+                $arr[$dest][$k]++;
+            }
+        } catch (Throwable $e) {
+            //
+            echo "Oops";
+        }
+        fclose($file);
+        foreach ($arr as &$i) {
+            ksort($i);
+        }
+        $f = fopen($outputPath, "w");
+        fwrite($f, "{\n");
+        foreach ($arr as $k => $dest) {
+            $x = str_replace("/", "\\/", $k);
+            fwrite($f, "    \"$x\": {\n");
+            foreach ($arr[$k] as $l => $v) {
+                fwrite($f, "        \"$l\": $v,\n");
+            }
+            fseek($f, -2, SEEK_CUR);
+            fwrite($f, "\n");
+            fwrite($f, "    },\n");
+        }
+        fseek($f, -2, SEEK_CUR);
+        fwrite($f, "\n");
+        fwrite($f, "}");
+        fclose($f);
     }
 }
