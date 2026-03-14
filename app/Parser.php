1fb568d85f546b52309229c0a9434d047d50690a
roey <roeycohen@gmail.com>
2026-03-14 16:57:26 +0200

    10M - ~5.6 sec 2

--- app/Parser.php
+++ app/Parser.php
@@ -6,10 +6,10 @@ final class Parser
 {
 	public function parse(string $inputPath, string $outputPath): void
 	{
+		gc_disable();
 		$posts = [];
 		$chunk_size = 16 * 1024;
 
-		$start = microtime(true);
 		$handle = fopen($inputPath, "r");
 		stream_set_read_buffer($handle, 0);
 		$last_line = '';
@@ -21,17 +21,21 @@ final class Parser
 			for ($i = 0; $i < $count; $i++)
 			{
 				if ($i === $count - 1)
+				{
 					$last_line = $lines[$i];
+				}
 				else
 				{
 					if ($i === 0)
 						$line = $last_line . $lines[$i];
 					else
 						$line = $lines[$i];
+
 					$path = substr($line, 19, -26);
 					$date = substr($line, -25, 10);
-
-			$posts[$path][] = $date;
+					$posts[$path][] = $date;
+				}
+			}
 		}
 		fclose($handle);
 		foreach ($posts as &$dates)
