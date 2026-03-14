1b6e646779412445af112be950afe3ce2cf05782
Marcos <pazzarelli.marcos@gmail.com>
2026-03-14 19:04:53 -0300

    .

--- app/Parser.php
+++ app/Parser.php
@@ -249,7 +249,7 @@ final class Parser
 
         $fmtUris = [];
         for ($u = 0; $u < $uriCount; $u++) {
-            $fmtUris[$u] = '"\/blog\/' . str_replace('/', '\/', $uriList[$u]) . '": {';
+            $fmtUris[$u] = '"\/blog\/' .  $uriList[$u] . '": {';
         }
 
         $sep = "\n    ";
